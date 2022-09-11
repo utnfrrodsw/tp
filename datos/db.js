52a0b3afdc566e0a7373f523e51a47e514e527ae
c3r38r170 <c3r38r170@gmail.com>
2022-09-11 20:10:57 -0300

    Security acknowledgement.

--- datos/db.js
+++ datos/db.js
@@ -1,6 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 module.exports= new Sequelize(
+  // TODO no ser un sucio y usar variables de entorno
   'mysql://3wt715xtrh0mchoinbvr:pscale_pw_W5GQfPT33CbNGoZSqW0snn4DtwQ9YBL0G71QOjuNF9s@us-east.connect.psdb.cloud/ttads'
   , {dialectOptions: {
     ssl: {
