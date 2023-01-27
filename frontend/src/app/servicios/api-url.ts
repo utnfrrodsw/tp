f8c33ff21b5b7fa48c6da56189d963ace44be9d1
c3r38r170 <c3r38r170@gmail.com>
2023-01-26 21:01:09 -0300

    Pruebas en backend local.

--- frontend/src/app/servicios/api-url.ts
+++ frontend/src/app/servicios/api-url.ts
@@ -1,1 +1,2 @@
-export const API_URL='https://ttads-fullstack.onrender.com/api/';
+// export const API_URL='https://ttads-fullstack.onrender.com/api/';
+export const API_URL='http://localhost:8080/api/';
