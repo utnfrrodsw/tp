c7485060539f78ea00bf6eb6edfc400dfecfb249
AgustinaRodriguez01 <agus4956@gmail.com>
2023-07-19 12:19:27 -0300

    Actualizacion header

--- TPI/src/app/app-routing.module.ts
+++ TPI/src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'home', component: HomeComponent },
+{ path: '', redirectTo: '/home', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
