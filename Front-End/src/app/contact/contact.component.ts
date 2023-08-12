1b452f38afcec6c0f7361a11983c8fca28187b4f
Alex <alejoretamal432@gmail.com>
2023-08-12 18:13:59 -0300

    Prototipo-v1
    Prototipo básico de la estructura del sitio.

---
+++ Front-End/src/app/contact/contact.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css']
+})
+export class ContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    
+  }
+
+}
---
+++ Front-End/src/app/registro/registro.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+export class RegistroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
