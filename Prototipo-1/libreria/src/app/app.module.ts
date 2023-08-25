988406089ae5b3cad4b04d1482e45bee2d8a54ec
Marina Milo <milomarina51@gmail.com>
2023-08-24 22:53:08 -0300

    Agregado componente de footer.

--- Prototipo-1/libreria/src/app/app.module.ts
+++ Prototipo-1/libreria/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { HeadernavComponent } from './headernav/headernav.component';
 import { SeccionOfertasComponent } from './seccion-ofertas/seccion-ofertas.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { SeccionOfertasComponent } from './seccion-ofertas/seccion-ofertas.compo
     UsuarioComponent,
     HeadernavComponent,
     SeccionOfertasComponent,
+    FooterComponent,
   ],
   imports: [BrowserModule],
   providers: [],
