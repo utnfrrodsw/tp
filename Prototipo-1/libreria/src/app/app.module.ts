eecc8e9adf6e84a47ba73080ef2b92e96a2b28ea
Marina Milo <milomarina51@gmail.com>
2023-08-24 22:25:59 -0300

    Agregado componente de sección de ofertas tipo carousel.

--- Prototipo-1/libreria/src/app/app.module.ts
+++ Prototipo-1/libreria/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { UsuarioComponent } from './usuario/usuario.component';
 import { HeadernavComponent } from './headernav/headernav.component';
+import { SeccionOfertasComponent } from './seccion-ofertas/seccion-ofertas.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { HeadernavComponent } from './headernav/headernav.component';
     UsuariosComponent,
     UsuarioComponent,
     HeadernavComponent,
+    SeccionOfertasComponent,
   ],
   imports: [BrowserModule],
   providers: [],
