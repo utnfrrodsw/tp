ce52642859ba54ff112db3c7fd68fb30de7f6913
Marina Milo <milomarina51@gmail.com>
2023-08-27 18:10:29 -0300

    Agregar sticky-top a header y navbar; al hacer scroll desaparece

--- Prototipo-1/libreria/src/app/headernav/headernav.component.ts
+++ Prototipo-1/libreria/src/app/headernav/headernav.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-headernav',
   templateUrl: './headernav.component.html',
-  styleUrls: ['./headernav.component.css']
+  styleUrls: ['./headernav.component.css'],
 })
 export class HeadernavComponent {
+  headerScrolled = false;
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.headerScrolled = window.scrollY > 0;
+  }
 }
