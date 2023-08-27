91da94b157d9202ae21a61b0ba7c9065af533f67
Marina Milo <milomarina51@gmail.com>
2023-08-27 17:51:38 -0300

    Agregar constante "nombre_sitio" en src/app/shared/constants.ts

--- Prototipo-1/libreria/src/app/app.component.ts
+++ Prototipo-1/libreria/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { nombre_sitio } from '../app/shared/constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'libreria';
+  nombre_sitio = nombre_sitio;
 }
--- Prototipo-1/libreria/src/app/footer/footer.component.ts
+++ Prototipo-1/libreria/src/app/footer/footer.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { nombre_sitio } from '../shared/constants';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
-
+  nombre_sitio = nombre_sitio;
 }
