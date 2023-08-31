b749961ddb8fa6472868d0866e774a90cb6aba3d
Agustin Obiedo <agus1000obiedo@gmail.com>
2023-08-30 22:35:50 -0300

    Modifico app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,16 @@
 import React /*, {useEffect, useState}*/ from 'react';
-
-import Home from './components/home/Home.jsx';
-import Header from './components/header/Header.jsx';
-import InicioCliente from './components/cliente/Inicio/InicioCliente.jsx';
-import Error from './components/error/Error.jsx';
-import Solicitudes from './components/cliente/solicitudes/Solicitudes.jsx';
-import Evaluaciones from './components/evaluaciones/Evaluaciones.jsx';
-import Footer from './components/footer/Footer.jsx';
-
-
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
+import Login from './components/auth/Login.jsx'; // Asegúrate de importar correctamente
+import Register from './components/auth/Register.jsx'; // Asegúrate de importar correctamente
+import Error from './components/error/Error.jsx';
+import Footer from './components/footer/Footer.jsx';
+import Header from './components/header/Header.jsx';
+import Home from './components/home/Home.jsx';
 
 function App() {
-
-  /*const [backendData, setBackendData] = useState([{}]);
-  
-  useEffect(() => {
-    fetch("http://localhost:5000/api/tasks")
-      .then(
-        response => response.json()
-      )
-      .then(
-        data => {
-          setBackendData(data)
-        })
-      .catch(
-        error => {
-        console.error('Error al obtener los datos:', error);
-      });
-  }, []);*/
+  /* ... */
 
   return (
     <div className="App">
@@ -38,11 +18,9 @@ function App() {
         <Header/>
         <Routes>
           <Route path='/'  element={ <Home/>} />
-          <Route path='/client/home'  element={ <InicioCliente/>}>
-            <Route path='requests'  element={ <Solicitudes estado = "pendiente"/>} />
-            <Route path='finished'  element={ <Solicitudes estado = "terminado"/>} />
-          </Route>
-          <Route path='/evaluations'  element={ <Evaluaciones/>} />
+          {/* ...otras rutas existentes ... */}
+          <Route path='/login' element={<Login />} /> {/* Ruta para iniciar sesión */}
+          <Route path='/register' element={<Register />} /> {/* Ruta para registrarse */}
           <Route path='*'  element={ <Error/>} />
         </Routes>
       </div>
@@ -52,3 +30,4 @@ function App() {
 }
 
 export default App;
+
