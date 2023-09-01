d04ee9d83a82f2fbc5f70c40bf439ee9d9b90700
Facundo <78052165+FacuTaborra@users.noreply.github.com>
2023-09-01 10:15:03 -0300

    cambio el nombre de ./home.css

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Home.css'; // Estilo para la página Home
+import './home.css'; // Estilo para la página Home
 
 function Home() {
   return (
