10e0cd120c8a9183552b9fc484928d39f4016a54
Bautista Guerra <bauguerra@gmail.com>
2023-09-03 12:29:59 -0300

    all routes protected except login and register
    all routes protected except login and register

--- tp-backend/nospeak-api-node/routes/apiRoutes.js
+++ tp-backend/nospeak-api-node/routes/apiRoutes.js
@@ -11,53 +11,53 @@ const playlistController = require('../controllers/playlistController');
 const recomendacionController = require('../controllers/recomendacionController');
 const historialController = require('../controllers/historialController');
 
-router.get('/usuarios', usuarioController.getUsuarios);
+router.get('/usuarios', verificarToken,usuarioController.getUsuarios);
 router.post('/usuarios', usuarioController.createUsuario);
-router.get('/usuarios/:id', usuarioController.getUsuarioById);
-router.put('/usuarios/:id', usuarioController.updateUsuario);
-router.delete('/usuarios/:id', usuarioController.deleteUsuario);
+router.get('/usuarios/:id', verificarToken, usuarioController.getUsuarioById);
+router.put('/usuarios/:id', verificarToken, usuarioController.updateUsuario);
+router.delete('/usuarios/:id', verificarToken, usuarioController.deleteUsuario);
 router.post('/usuarios-login/', usuarioController.loginUsuario);
 
 
-router.get('/artistas', artistaController.getArtistas);
-router.post('/artistas', artistaController.createArtista);
-router.get('/artistas/:id', artistaController.getArtistaById);
-router.put('/artistas/:id', artistaController.updateArtista);
-router.delete('/artistas/:id', artistaController.deleteArtista);
+router.get('/artistas', verificarToken, artistaController.getArtistas);
+router.post('/artistas', verificarToken, artistaController.createArtista);
+router.get('/artistas/:id', verificarToken, artistaController.getArtistaById);
+router.put('/artistas/:id', verificarToken, artistaController.updateArtista);
+router.delete('/artistas/:id', verificarToken, artistaController.deleteArtista);
 
-router.get('/albums', albumController.getAlbums);
-router.post('/albums', albumController.createAlbum);
-router.get('/albums/:id', albumController.getAlbumById);
-router.put('/albums/:id', albumController.updateAlbum);
-router.delete('/albums/:id', albumController.deleteAlbum);
+router.get('/albums', verificarToken, albumController.getAlbums);
+router.post('/albums', verificarToken, albumController.createAlbum);
+router.get('/albums/:id', verificarToken, albumController.getAlbumById);
+router.put('/albums/:id', verificarToken, albumController.updateAlbum);
+router.delete('/albums/:id', verificarToken, albumController.deleteAlbum);
 
 router.get('/canciones', verificarToken, cancionController.getCanciones);
-router.post('/canciones', cancionController.createCancion);
-router.get('/canciones/:id', cancionController.getCancionById);
-router.put('/canciones/:id', cancionController.updateCancion);
-router.delete('/canciones/:id', cancionController.deleteCancion);
-router.get('/canciones-artista/:artista_id', cancionController.getCancionesPorArtista);
-router.get('/canciones-album/:album_id', cancionController.getCancionesPorAlbum);
+router.post('/canciones', verificarToken, cancionController.createCancion);
+router.get('/canciones/:id', verificarToken, cancionController.getCancionById);
+router.put('/canciones/:id', verificarToken, cancionController.updateCancion);
+router.delete('/canciones/:id', verificarToken, cancionController.deleteCancion);
+router.get('/canciones-artista/:artista_id', verificarToken, cancionController.getCancionesPorArtista);
+router.get('/canciones-album/:album_id', verificarToken ,cancionController.getCancionesPorAlbum);
 
 
-router.get('/playlists', playlistController.getPlaylists);
-router.post('/playlists', playlistController.createPlaylist);
-router.get('/playlists/:id', playlistController.getPlaylistById);
-router.put('/playlists/:id', playlistController.updatePlaylist);
-router.delete('/playlists/:id', playlistController.deletePlaylist);
-router.get('/playlists-usuario/:usuario_id', playlistController.getPlaylistsByUsuario);
+router.get('/playlists', verificarToken, playlistController.getPlaylists);
+router.post('/playlists', verificarToken, playlistController.createPlaylist);
+router.get('/playlists/:id', verificarToken, playlistController.getPlaylistById);
+router.put('/playlists/:id', verificarToken, playlistController.updatePlaylist);
+router.delete('/playlists/:id', verificarToken, playlistController.deletePlaylist);
+router.get('/playlists-usuario/:usuario_id', verificarToken, playlistController.getPlaylistsByUsuario);
 
 
-router.get('/recomendaciones', recomendacionController.getRecomendaciones);
-router.post('/recomendaciones', recomendacionController.createRecomendacion);
-router.get('/recomendaciones/:id', recomendacionController.getRecomendacionById);
-router.put('/recomendaciones/:id', recomendacionController.updateRecomendacion);
-router.delete('/recomendaciones/:id', recomendacionController.deleteRecomendacion);
+router.get('/recomendaciones', verificarToken, recomendacionController.getRecomendaciones);
+router.post('/recomendaciones', verificarToken, recomendacionController.createRecomendacion);
+router.get('/recomendaciones/:id', verificarToken, recomendacionController.getRecomendacionById);
+router.put('/recomendaciones/:id', verificarToken, recomendacionController.updateRecomendacion);
+router.delete('/recomendaciones/:id', verificarToken, recomendacionController.deleteRecomendacion);
 
-router.get('/historiales', historialController.getHistoriales);
-router.post('/historiales', historialController.createHistorial);
-router.get('/historiales/:id', historialController.getHistorialById);
-router.put('/historiales/:id', historialController.updateHistorial);
-router.delete('/historiales/:id', historialController.deleteHistorial);
+router.get('/historiales', verificarToken, historialController.getHistoriales);
+router.post('/historiales', verificarToken, historialController.createHistorial);
+router.get('/historiales/:id', verificarToken, historialController.getHistorialById);
+router.put('/historiales/:id', verificarToken, historialController.updateHistorial);
+router.delete('/historiales/:id', verificarToken, historialController.deleteHistorial);
 
 module.exports = router;
