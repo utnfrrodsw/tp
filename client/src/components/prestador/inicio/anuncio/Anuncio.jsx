50444daa441b33a0ac83c352becb4e3ecbe3a8ad
NahuelBuschittari <nahubuschittari0310@gmail.com>
2023-09-12 14:59:26 -0300

    Carousel funcionando con bootstrap, falta acomodarlo

--- client/src/components/prestador/inicio/anuncio/Anuncio.jsx
+++ client/src/components/prestador/inicio/anuncio/Anuncio.jsx
@@ -1,7 +1,8 @@
 import './anuncio.css'
 import { Link } from 'react-router-dom';
 import React, { useState } from 'react';
-import ControlledCarousel from './carousel';
+import ControlledCarousel from './carousel'
+
 function Anuncio(props){
   const titulo=props.titulo;
   const descripcion=props.descripcion;
@@ -10,17 +11,16 @@ function Anuncio(props){
   const [show,setShow]=useState(true);
   return(
   <div className={`anuncio-card ${show ? "anuncio-card" : "anuncio-fullcontent"}`}>
-    <div className='photo'>
-      <img className='img' src='https://unavatar.io/kikobeats?ttl=1h' alt="not Found" />
-    </div>
     <div className='titulo'><h1>{titulo}</h1></div>
     <div className='descripcion'>{descripcion}</div>
     <div className='nombre'>{nombre}</div>
     <button className='boton' onClick={()=> {setShow(!show);}}>Ver {show ? 'más':'menos'}</button>
     {show ? (<h1></h1>
     ):(
-        <button className='presu'>Presupuestar</button>
-        //<ControlledCarousel/>
+        <>
+          <button className='presu'>Presupuestar</button>
+          <div className='photo'><ControlledCarousel /></div>
+        </>
     )}
   </div>
 );
---
+++ client/src/components/prestador/inicio/anuncio/carousel.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import './anuncio.css'
+
+function ControlledCarousel() {
+  const [index, setIndex] = useState(0);
+
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <Carousel activeIndex={index} onSelect={handleSelect}>
+      <Carousel.Item>
+        <img className='img' src="https://unavatar.io/kikobeats?ttl=1h" text="First slide" />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='img' src="https://unavatar.io/kikobeats?ttl=1h"text="Second slide" />
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img className='img' src="https://unavatar.io/kikobeats?ttl=1h" text="Third slide" />
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
+}
+
+export default ControlledCarousel;
