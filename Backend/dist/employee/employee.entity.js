9121ba4c7f987c4b94ada9075f4467ee23968b66
oldaniMarcos <139399407+oldaniMarcos@users.noreply.github.com>
2023-09-14 16:26:07 -0300

    add gitignore

--- Backend/dist/employee/employee.entity.js
+++ Backend/dist/employee/employee.entity.js
@@ -1,12 +1,12 @@
 import crypto from 'node:crypto';
 export class Employee {
-    constructor(name, email, phoneNum, address, salary, idEmp = crypto.randomUUID()) {
+    constructor(name, email, phoneNum, address, salary, id = crypto.randomUUID()) {
         this.name = name;
         this.email = email;
         this.phoneNum = phoneNum;
         this.address = address;
         this.salary = salary;
-        this.idEmp = idEmp;
+        this.id = id;
     }
 }
 //# sourceMappingURL=employee.entity.js.map
