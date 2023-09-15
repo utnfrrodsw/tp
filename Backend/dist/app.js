c131f3c72117198e132272b9a489afd7ea65d566
oldaniMarcos <139399407+oldaniMarcos@users.noreply.github.com>
2023-09-15 08:12:56 -0300

    quick fix

--- Backend/dist/app.js
+++ Backend/dist/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { employeeRouter } from './employee/employee.routes.js';
+import { clientRouter } from './client/client.routes.js';
 const app = express();
 app.use(express.json());
 app.use('/api/employees', employeeRouter);
+app.use('/api/clients', clientRouter);
 app.use((_, res) => {
     return res.status(404).send({ message: 'Resource not found' });
 });
