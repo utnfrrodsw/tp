99c64a04495dbbe70fd11cece8fced4a98047fac
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-09-30 17:27:59 -0300

    arreglo update

--- src/routes/UserRouter.mjs
+++ src/routes/UserRouter.mjs
@@ -1,6 +1,6 @@
 import express from 'express';
 /* import funciones from '../controllers/UserController.js' */
-import {createUser, login, getAll, UpdateUser, UpdateUserPassword, getUserByToken } from '../controllers/UserController.mjs'
+import {createUser, login, getAll, UpdateUser, UpdateUserPassword, getUserByToken,getUserRole } from '../controllers/UserController.mjs'
 import {verifyTokenUser, verifyTokenAdmin } from '../middlewares/token.mjs'
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.get('/users', verifyTokenUser, getAll);
 router.get('/userbytoken', verifyTokenUser, getUserByToken);
 router.put('/user-info', verifyTokenUser, UpdateUser);
 router.put('/user-password', verifyTokenUser, UpdateUserPassword);
+router.get('/user/role', verifyTokenUser ,getUserRole);
 
 export default router;
