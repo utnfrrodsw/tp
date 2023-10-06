58360eafb068d2ffef674a4d0c87168f8a3f92aa
FacuTaborra <taborrafacundo@gmail.com>
2023-10-06 10:32:29 -0300

    actualizo rutas

--- server/src/routes/solicitudRoutes.js
+++ server/src/routes/solicitudRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const solicitudController = require( "../../controllers/solicitud/solicitudController.js");
+const solicitudController = require( "../controllers/solicitud/solicitudController.js");
 
 router.get('/:id',solicitudController.getSolicitud);
 
