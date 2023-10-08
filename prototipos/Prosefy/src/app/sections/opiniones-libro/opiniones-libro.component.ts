32404d9784131fd79fb7b5f75d7e1d1b1efc50d2
Marina Milo <milomarina51@gmail.com>
2023-10-07 22:55:03 -0300

    Agregar apartado de opiniones en libro seleccionado - Agregar calificación y estrellas responsivo - Ajustar contenedor para que el ancho sea apropiado respecto de los demás contenedores

--- prototipos/Prosefy/src/app/sections/opiniones-libro/opiniones-libro.component.ts
+++ prototipos/Prosefy/src/app/sections/opiniones-libro/opiniones-libro.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Libro, LibrosService } from '../../services/libros.service';
 
 @Component({
   selector: 'app-opiniones-libro',
   templateUrl: './opiniones-libro.component.html',
-  styleUrls: ['./opiniones-libro.component.css']
+  styleUrls: ['./opiniones-libro.component.css'],
 })
-export class OpinionesLibroComponent {
+export class OpinionesLibroComponent implements OnInit {
+  libro: Libro | undefined;
 
+  constructor(
+    private librosService: LibrosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    // Obtener el valor del parámetro ":id" desde la URL
+    this.route.paramMap.subscribe((params) => {
+      const idParam = params.get('id');
+
+      if (idParam !== null) {
+        const libroId = parseInt(idParam, 10);
+
+        // Obtener el libro por ID utilizando el servicio de libros
+        this.libro = this.librosService.getLibroById(libroId);
+      } else {
+        // Redirigir a la página de inicio
+        this.router.navigate(['/inicio']);
+      }
+    });
+  }
 }
