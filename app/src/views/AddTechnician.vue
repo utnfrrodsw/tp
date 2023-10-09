11ec7d502b163a3288af5790650916fdc6de1357
andres guerrero <andresguerreroutn@gmail.com>
2023-10-09 16:12:08 -0300

    add validations technicians

--- app/src/views/AddTechnician.vue
+++ app/src/views/AddTechnician.vue
@@ -1,40 +1,49 @@
 <template>
   <v-container class="my-10">
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-text-field v-model="tecnico.name" label="Nombre" required></v-text-field>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-menu
-          v-model="menu"
-          :close-on-content-click="false"
-          :nudge-right="40"
-          transition="scale-transition"
-          offset-y
-          min-width="auto"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="tecnico.date_born"
-              label="Fecha de nacimiento"
-              prepend-icon="mdi-calendar"
-              readonly
-              required
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="tecnico.date_born" @input="menu = false"></v-date-picker>
-        </v-menu>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-btn color="primary" :loading="loading" @click="postTecnico">
-          <v-icon left>mdi-account-plus</v-icon>
-          Agregar técnico
-        </v-btn>
-      </v-col>
-    </v-row>
+    <v-form ref="form" class="mx-2" lazy-validation>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field 
+            v-model="tecnico.name" 
+            label="Nombre" 
+            :rules="validationRules.name"
+            required
+          >
+          </v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-menu
+            v-model="menu"
+            :close-on-content-click="false"
+            :nudge-right="40"
+            transition="scale-transition"
+            offset-y
+            min-width="auto"
+          >
+            <template v-slot:activator="{ on }">
+              <v-text-field
+                v-model="tecnico.date_born"
+                label="Fecha de nacimiento"
+                prepend-icon="mdi-calendar"
+                :rules="validationRules.date_born"
+                readonly
+                required
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker v-model="tecnico.date_born" @input="menu = false"></v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-btn color="primary" :loading="loading" @click="submitForm">
+            <v-icon left>mdi-account-plus</v-icon>
+            Agregar técnico
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
 
     <v-row v-if="alert.show">
       <v-col>
@@ -59,6 +68,15 @@ export default {
         name: "",
         date_born: "",
       },
+      validationRules: {
+        name: [
+          v => !!v || 'Nombre is required',
+          v => (v && v.length <= 10) || 'Nombre must be less than 10 characters',
+        ],
+        date_born: [
+          v => !!v || 'Fecha de nacimiento is required',
+        ],
+      },
       loading: false,
       alert: {
         show: false,
@@ -69,29 +87,37 @@ export default {
     };
   },
   methods: {
-    async postTecnico() {
-      this.loading = true;
-
-      try {
-        const body = {
-          name: this.tecnico.name,
-          date_born: this.tecnico.date_born,
-        };
-        const response = await axios.post(`http://localhost:4000/api/technicians`, body)
-        const data = await response.data;
-
-        this.alert.show = true;
-        this.alert.message = "Tecnico creado correctamente";
-        this.alert.type = "success";
-
-        this.tecnico.name = "";
-        this.tecnico.date_born = "";
-      } catch (e) {
-        this.alert.show = true;
-        this.alert.message = "Error al agregar técnico";
-        this.alert.type = "error";
+    async submitForm() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+        try {
+          const body = {
+            name: this.tecnico.name,
+            date_born: this.tecnico.date_born,
+          };
+          const response = await axios.post(`http://localhost:4000/api/technicians`, body)
+          const data = await response.data;
+  
+          this.alert.show = true;
+          this.alert.message = "Tecnico creado correctamente";
+          this.alert.type = "success";
+  
+          this.reset()
+          this.resetValidation()
+          this
+        } catch (e) {
+          this.alert.show = true;
+          this.alert.message = "Error al agregar técnico";
+          this.alert.type = "error";
+        }
+        this.loading = false;
       }
-      this.loading = false;
+    },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
     }
   },
 };
--- app/src/views/EditTechnician.vue
+++ app/src/views/EditTechnician.vue
@@ -1,23 +1,35 @@
 <template>
   <v-container fluid>
-    <v-row>
-      <v-col cols="12" md="6">
-        <v-text-field v-model="tecnico.name" label="Nombre"></v-text-field>
-      </v-col>
-      <v-col cols="12" md="6">
-        <v-menu v-model="menu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y>
-          <template v-slot:activator="{ on, attrs }">
-            <v-text-field v-model="formattedDate" label="Fecha de Nacimiento" readonly v-bind="attrs" v-on="on"></v-text-field>
-          </template>
-          <v-date-picker v-model="tecnico.date_born" @input="menu = false"></v-date-picker>
-        </v-menu>
-      </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="12">
-        <v-btn color="primary" @click="putTecnico">Guardar Cambios</v-btn>
-      </v-col>
-    </v-row>
+    <v-form ref="form" class="mx-2" lazy-validation>
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-text-field 
+            v-model="tecnico.name" 
+            :rules="validationRules.name"
+            label="Nombre"
+          >
+        </v-text-field>
+        </v-col>
+        <v-col cols="12" md="6">
+          <v-menu v-model="menu" :close-on-content-click="false" :nudge-right="40" transition="scale-transition" offset-y>
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field v-model="formattedDate" label="Fecha de Nacimiento" readonly v-bind="attrs" v-on="on"></v-text-field>
+            </template>
+            <v-date-picker 
+              v-model="tecnico.date_born" 
+              :rules="validationRules.date_born"
+              @input="menu = false"
+            >
+            </v-date-picker>
+          </v-menu>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="12">
+          <v-btn color="primary" @click="submitForm">Guardar Cambios</v-btn>
+        </v-col>
+      </v-row>
+    </v-form>
     <alerts v-if="alert.show" :title="alert.title" :message="alert.message" @salir="redirectList"></alerts>
   </v-container>
 </template>
@@ -37,6 +49,15 @@ export default {
         id: this.$route.params.id,
         name: "",
         date_born: null,
+      },
+      validationRules: {
+        name: [
+          v => !!v || 'Nombre is required',
+          v => (v && v.length <= 10) || 'Nombre must be less than 10 characters',
+        ],
+        date_born: [
+          v => !!v || 'Fecha de nacimiento is required',
+        ],
       },
       menu: false,
       alert: {
@@ -73,34 +94,42 @@ export default {
       }
       this.loading = false
     },
-    async putTecnico() {
-      this.loading = true;
-
-      try {
-        const body = {
-          name: this.tecnico.name,
-          date_born: this.tecnico.date_born,
-        };
-        const response = await axios.put(`http://localhost:4000/api/technicians/${this.tecnico.id}`, body);
-        const data = await response.data;
-        this.alert.show = true;
-        this.alert.title = "Guardado exitoso";
-        this.alert.message = "El cambio se realizó exitosamente";
-        this.alert.type = "success";
-
-        this.tecnico.name = "";
-        this.tecnico.date_born = "";
-      } catch (error) {
-        this.alerta.show = true;
-        this.alerta.title = "Error";
-        this.alerta.mensaje = "Error al agregar técnico";
-        this.alerta.type = "error";
+    async submitForm() {
+      if (this.$refs.form.validate()) {
+        this.loading = true;
+  
+        try {
+          const body = {
+            name: this.tecnico.name,
+            date_born: this.tecnico.date_born,
+          };
+          const response = await axios.put(`http://localhost:4000/api/technicians/${this.tecnico.id}`, body);
+          const data = await response.data;
+          this.alert.show = true;
+          this.alert.title = "Guardado exitoso";
+          this.alert.message = "El cambio se realizó exitosamente";
+          this.alert.type = "success";
+  
+          this.reset()
+          this.resetValidation()
+        } catch (error) {
+          this.alerta.show = true;
+          this.alerta.title = "Error";
+          this.alerta.mensaje = "Error al agregar técnico";
+          this.alerta.type = "error";
+        }
+        this.loading = false;
       }
-      this.loading = false;
     },
     redirectList() {
       this.$router.push({ path: '/list-technicians' }).catch(() => {});
     },
+    reset () {
+      this.$refs.form.reset()
+    },
+    resetValidation () {
+      this.$refs.form.resetValidation()
+    }
   },
 };
 </script>
