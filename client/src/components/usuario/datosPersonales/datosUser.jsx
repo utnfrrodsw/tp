7ddc7fbcc96a12f32a5503b3ef67da1f8c804258
FacuTaborra <taborrafacundo@gmail.com>
2023-10-12 11:28:07 -0300

    agregue singout en el front

--- client/src/components/usuario/datosPersonales/datosUser.jsx
+++ client/src/components/usuario/datosPersonales/datosUser.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { Button, Card, Col, Row } from 'react-bootstrap';
 import { useAuth } from '../../../auth/authProvider';
+import { API_URL } from '../../../auth/constants.js';
 import './datosUser.css';
 
 
@@ -45,108 +47,130 @@ const DatosPersonales = () => {
     setFotoPerfil(URL.createObjectURL(file));
   };
 
-  return (
-    <Row>
-      <Col>
-        <Card className='cardDatosPer'>
-          <Card.Body>
-            <h2 className='h2'>Datos Personales</h2>
-            <div className="user-details">
-              <div className="profile-picture">
-                {fotoPerfil ? (
-                  <img src={fotoPerfil} alt="Foto de perfil" />
-                ) : (
-                  <img src='./128-1280406_view-user-icon-png-user-circle-icon-png.png' alt="Foto de perfil por defecto" />
-                )}
-                <input
-                  type="file"
-                  accept="image/*"
-                  onChange={handleProfilePictureChange}
-                />
-              </div>
-              <label htmlFor="firstName">Nombre:</label>
-              <input
-                type="text"
-                id="firstName"
-                name="firstName"
-                value={userData.nombre}
-                onChange={(e) => setUserData({ ...userData, nombre: e.target.value })}
-              />
-              <label htmlFor="lastName">Apellido:</label>
-              <input
-                type="text"
-                id="lastName"
-                name="lastName"
-                value={userData.apellido}
-                onChange={(e) => setUserData({ ...userData, apellido: e.target.value })}
-              />
-              <label htmlFor="email">Correo Electrónico:</label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={userData.email}
-                onChange={(e) => setUserData({ ...userData, email: e.target.value })}
-              />
-              <label htmlFor="birthdate">Fecha de Nacimiento:</label>
-              <input
-                type="date"
-                id="birthdate"
-                name="birthdate"
-                value={userData.fechaNacimiento}
-                onChange={(e) => setUserData({ ...userData, fechaNacimiento: e.target.value })}
-              />
+  // Función para cerrar sesión
+  async function handlesignout(e){
+    e.preventDefault();
+    try {
+      const response = await fetch(`${API_URL}/signout`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${auth.getRefreshToken()}`,
+        },
+      });
+      if (response.ok) {
+        auth.signout();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-              <Button variant="primary" className='button' onClick={handleUpdateData}>
-                Actualizar Datos
-              </Button>
-            </div>
-          </Card.Body>
-        </Card>
-      </Col>
-       
-      <Col>
-        <Card className='cardSegurity'>
-          <Card.Body>
-            <h2 className='h2'>Cambiar Contraseña</h2>
-            <div className="security-details">
-              <label htmlFor="currentPassword">Contraseña Actual:</label>
-              <input
-                type="password"
-                id="currentPassword"
-                name="currentPassword"
-                value={contrasenaActual}
-                onChange={(e) => setContrasenaActual(e.target.value)}
-              />
-              <Button variant="primary" className='button' onClick={verifyCurrentPassword}>
-                Verificar Contraseña
-              </Button>
-              {contrasenaActual && (
-                <>
-                  <label htmlFor="newPassword">Nueva Contraseña:</label>
+  return (
+    <div>
+      <Row>
+        <Col>
+          <Card className='cardDatosPer'>
+            <Card.Body>
+              <h2 className='h2'>Datos Personales</h2>
+              <div className="user-details">
+                <div className="profile-picture">
+                  {fotoPerfil ? (
+                    <img src={fotoPerfil} alt="Foto de perfil" />
+                  ) : (
+                    <img src='./128-1280406_view-user-icon-png-user-circle-icon-png.png' alt="Foto de perfil por defecto" />
+                  )}
                   <input
-                    type="password"
-                    id="newPassword"
-                    name="newPassword"
-                    value={nuevaContrasena}
-                    onChange={(e) => setNuevaContrasena(e.target.value)}
+                    type="file"
+                    accept="image/*"
+                    onChange={handleProfilePictureChange}
                   />
-                  <label htmlFor="confirmNewPassword">Confirmar Nueva Contraseña:</label>
-                  <input
-                    type="password"
-                    id="confirmNewPassword"
-                    name="confirmNewPassword"
-                    value={confirmNuevaContrasena}
-                    onChange={(e) => setConfirmNuevaContrasena(e.target.value)}
-                  />
-                  <Button variant="primary"  className='button'>Cambiar Contraseña</Button>
-                </>
-              )}
-            </div>
-          </Card.Body>
-        </Card>
-      </Col>
-    </Row>
+                </div>
+                <label htmlFor="firstName">Nombre:</label>
+                <input
+                  type="text"
+                  id="firstName"
+                  name="firstName"
+                  value={userData.nombre}
+                  onChange={(e) => setUserData({ ...userData, nombre: e.target.value })}
+                />
+                <label htmlFor="lastName">Apellido:</label>
+                <input
+                  type="text"
+                  id="lastName"
+                  name="lastName"
+                  value={userData.apellido}
+                  onChange={(e) => setUserData({ ...userData, apellido: e.target.value })}
+                />
+                <label htmlFor="email">Correo Electrónico:</label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={userData.email}
+                  onChange={(e) => setUserData({ ...userData, email: e.target.value })}
+                />
+                <label htmlFor="birthdate">Fecha de Nacimiento:</label>
+                <input
+                  type="date"
+                  id="birthdate"
+                  name="birthdate"
+                  value={userData.fechaNacimiento}
+                  onChange={(e) => setUserData({ ...userData, fechaNacimiento: e.target.value })}
+                />
+
+                <Button variant="primary" className='button' onClick={handleUpdateData}>
+                  Actualizar Datos
+                </Button>
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+        
+        <Col>
+          <Card className='cardSegurity'>
+            <Card.Body>
+              <h2 className='h2'>Cambiar Contraseña</h2>
+              <div className="security-details">
+                <label htmlFor="currentPassword">Contraseña Actual:</label>
+                <input
+                  type="password"
+                  id="currentPassword"
+                  name="currentPassword"
+                  value={contrasenaActual}
+                  onChange={(e) => setContrasenaActual(e.target.value)}
+                />
+                <Button variant="primary" className='button' onClick={verifyCurrentPassword}>
+                  Verificar Contraseña
+                </Button>
+                {contrasenaActual && (
+                  <>
+                    <label htmlFor="newPassword">Nueva Contraseña:</label>
+                    <input
+                      type="password"
+                      id="newPassword"
+                      name="newPassword"
+                      value={nuevaContrasena}
+                      onChange={(e) => setNuevaContrasena(e.target.value)}
+                    />
+                    <label htmlFor="confirmNewPassword">Confirmar Nueva Contraseña:</label>
+                    <input
+                      type="password"
+                      id="confirmNewPassword"
+                      name="confirmNewPassword"
+                      value={confirmNuevaContrasena}
+                      onChange={(e) => setConfirmNuevaContrasena(e.target.value)}
+                    />
+                    <Button variant="primary"  className='button'>Cambiar Contraseña</Button>
+                  </>
+                )}
+              </div>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+      <Link className='ButtonCerrarSesion' onClick={handlesignout}>Cerrar Sesión</Link>
+    </div>
   );
 };
 
