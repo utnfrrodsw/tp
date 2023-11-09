45e1d8aede98c7785005cf81e394dfc3b5b10e4f
JulianSandoval25 <juliansandoval1996@gmail.com>
2023-11-09 14:55:42 -0300

    prueba prod

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,9 @@ import cors from "cors";
 import dotenv from "dotenv";
 import { dbConnect } from "./db.mjs";
 dotenv.config();
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 //import rutas
 import routerUser from './routes/UserRouter.mjs'
 import routerCategory from './routes/ProductCatRouter.mjs'
@@ -18,6 +21,10 @@ app.use(express.json());
 //Configuracion de archivos estaticos
 app.use("/uploads", express.static("src/uploads"));
 
+/* const isProduction= process.env.NODE_ENV === "production";
+const distPatch = isProduction ? "../dist/prod" : "../dist/dev"; */
+const distPatch = "../dist/dev"
+
 //Rutas
 app.use('/api', routerUser);
 app.use('/api', routerCategory);
@@ -26,6 +33,7 @@ app.use('/api', routerProduct);
 app.use('/api', routerReview);
 app.use('/api', routerCart);
 
+app.use(express.static(path.join(__dirname, distPatch)));
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
