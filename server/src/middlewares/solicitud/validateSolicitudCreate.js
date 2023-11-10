fecc369c4f3f5a22830ade4a7ff850a6a7324348
FacuTaborra <taborrafacundo@gmail.com>
2023-11-10 17:22:39 -0300

    agrego validaciones de solicitud

---
+++ server/src/middlewares/solicitud/validateSolicitudCreate.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const { check } = require("express-validator");
+const {validateResult} = require('../validateHelper.js');
+
+const validateSolicitudCreate = [
+    check("titulo")
+    .notEmpty()
+    .withMessage("El titulo es requerido")
+    .isLength({ min: 4 })   
+    .withMessage("El titulo debe tener al menos 4 caracteres"),
+    check("descripcion")
+    .notEmpty()
+    .withMessage("Debe explicar brevemente su solicitud")
+    .isLength({ min: 20 })
+    .withMessage("La descripcion debe tener al menos 20 caracteres"),
+    check("idProfesion")
+    .notEmpty()
+    .withMessage("Debe seleccionar una profesion"),
+    check("idDireccion")
+    .notEmpty()
+    .withMessage("Debe seleccionar una direccion, puede agregarlo en sus datos personales"),
+    check("fotos").custom((value, { req }) => {
+        let files = req.files;
+        let acceptedExtensions = [".jpg", ".png", ".gif", ".jpeg"];
+        console.log(files);
+        if (files.length == 0 ) {
+          throw new Error("Tiene que subir al menos una imagen");
+        } else if (files.length <= 5) {
+            files.map((file) => {
+                let fileExtension = path.extname(file.originalname);
+                if (!acceptedExtensions.includes(fileExtension)) {
+                  throw new Error(
+                    `Las extensiones de archivo permitidas son ${acceptedExtensions.join(
+                      ", "
+                    )}`
+                  );
+                }
+            });
+        }else if(files.length > 5){
+            throw new Error('Solo puede subir hasta 5 imagenes');
+        }
+        return true;
+    }),
+    (req, res, next) => {
+        validateResult(req, res, next);
+    },
+];
+
+module.exports = {validateSolicitudCreate};
