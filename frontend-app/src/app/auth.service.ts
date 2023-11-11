db9c0d0503b5875ac1538504efed109fc21c75f2
Federico <hulalaoooo@gmail.com>
2023-11-11 16:40:45 -0300

    features

--- frontend-app/src/app/auth.service.ts
+++ frontend-app/src/app/auth.service.ts
@@ -2,12 +2,13 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { jwtConstants } from 'auth.config';
-import { Observable, map } from 'rxjs';
+import { Observable, Subject, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  private loginSuccessSubject = new Subject<void>();
   constructor(private http: HttpClient, public jwtHelper: JwtHelperService) {}
 
   login(username: string, password: string): Observable<any> {
@@ -17,9 +18,14 @@ export class AuthService {
         map((response) => {
           const token = response.access_token;
           localStorage.setItem(jwtConstants.access_token, token);
+          this.loginSuccessSubject.next();
           return response;
         })
       );
+  }
+
+  onLoginSuccess(): Observable<void> {
+    return this.loginSuccessSubject.asObservable();
   }
 
   getRolSession(): string | null {
@@ -27,6 +33,15 @@ export class AuthService {
     const tokenDecoded = accessToken ? this.jwtHelper.decodeToken(accessToken) : null;
     if(tokenDecoded){
       return tokenDecoded.rol;
+    }
+    return null;
+  }
+
+  getUsername(): string | null {
+    const accessToken = localStorage.getItem(jwtConstants.access_token);
+    const tokenDecoded = accessToken ? this.jwtHelper.decodeToken(accessToken) : null;
+    if(tokenDecoded){
+      return `${tokenDecoded.firstname} ${tokenDecoded.lastname}`;
     }
     return null;
   }
--- frontend-app/src/app/shared/header/header.component.ts
+++ frontend-app/src/app/shared/header/header.component.ts
@@ -1,14 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
 
 @Component({
   selector: 'app-header',
   template: `
   <div class="shared-container fix-header">
     <div>Recomendador de inversiones</div>
+    <span class="user">{{username}}
+      <button
+      *ngIf="username != 'anonimo'"  
+      mat-icon-button 
+      color="warn"
+      (click)="logout()">
+        <mat-icon class="icon-align">close</mat-icon>
+      </button>
+    </span>
   </div>
   `,
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+  username: string = 'anonimo';
+  private loginSuccessSubscription?: Subscription;
+  constructor(private authService: AuthService) {
+    this.getUsername();
+  }
+  ngOnInit(): void {
+    this.loginSuccessSubscription = this.authService.onLoginSuccess().subscribe(() => {
+      this.getUsername();
+    });
+  }
+  
+  logout() {
+    this.authService.logout();
+    this.username = 'anonimo';
+  }
 
+  getUsername() {
+    const user = this.authService.getUsername();
+    if(user) {
+      this.username = user;
+    }
+  }
+
+  ngOnDestroy() {
+    this.loginSuccessSubscription?.unsubscribe();
+  }
 }
