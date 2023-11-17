0723894dbf70ecb889ca356f267e7f87d1cc1a4e
Ivan <ivanbisc12@gmail.com>
2023-11-17 17:58:23 -0300

    Correccion de doble conexion a bd

--- src/controllers/ShoppingCartController.mjs
+++ src/controllers/ShoppingCartController.mjs
@@ -1,6 +1,5 @@
 import Product from "../models/ProductModel.mjs";
 import Cart from "../models/ShoppingCart.mjs";
-import conn from "../db.mjs";
 
 class ReqError extends Error {
     constructor(message, httpCode = 500, ...args) {
@@ -9,52 +8,52 @@ class ReqError extends Error {
         this.message = message;
     }
 }
-
 export const saveCart = async (req, res) => {
-    const session = await conn.startSession();
+    const session = await Product.startSession();
     const transactionOptions = {
         readPreference: 'primary',
         readConcern: { level: 'local' },
         writeConcern: { w: 'majority' }
     };
+
     try {
-        const comprador = req.userID;
-        const { productos } = req.body;
-        console.log({ comprador, productos });
-        session.startTransaction(transactionOptions);
-        if (!comprador || !productos) {
-            throw new ReqError('Solicitud incorrecta, no es posible registrar compra', 400);
-        }
-        for (const linea of productos) {
-            if (!linea.producto) {
+        await session.withTransaction(async () => {
+            const comprador = req.userID;
+            const { productos } = req.body;
+            //console.log({ comprador, productos });
+            if (!comprador || !productos) {
                 throw new ReqError('Solicitud incorrecta, no es posible registrar compra', 400);
             }
-            let prod = await Product.findById(linea.producto);
-            if (!prod) {
-                throw new ReqError('Solicitud incorrecta, no es posible registrar compra', 400);
+            for (const linea of productos) {
+                if (!linea.producto) {
+                    throw new ReqError('Solicitud incorrecta, no es posible registrar compra', 400);
+                }
+                let prod = await Product.findById(linea.producto);
+                if (!prod) {
+                    throw new ReqError('Solicitud incorrecta, no es posible registrar compra', 400);
+                }
+                prod.stock -= linea.cantidad;
+                if (prod.stock < 0) {
+                    throw new ReqError(`No es posible realizar la compra, no hay stock de ${prod.nombre}`, 409);
+                }
+                linea.precio = prod.precio;
+                await prod.save();
             }
-            prod.stock -= linea.cantidad;
-            if (prod.stock < 0) {
-                throw new ReqError(`No es posible realizar la compra, no hay stock de ${prod.nombre}`, 409);
-            }
-            linea.precio = prod.precio;
-            await prod.save({ session });
-        }
-        //const Compra = new Cart({ comprador, productos });
-        //await Compra.save({ session });
-        Cart.create([{ comprador, productos }], session);
-        await session.commitTransaction();
-        res.status(201).json({ message: "Compra registrada" });
+            Cart.create([{ comprador, productos }], session);
+            res.status(201).json({ message: "Compra registrada" })
+
+        }, transactionOptions)
+
+
     } catch (error) {
-        await session.abortTransaction();
+        //console.log(error)
         if (!(error instanceof ReqError)) {
             error.httpCode = 500;
             error.message = "Error al procesar la compra";
         }
         res.status(error.httpCode).json({
+
             message: error.message
         });
-    } finally {
-        await session.endSession();
     }
 };
--- src/db.mjs
+++ src/db.mjs
@@ -13,12 +13,3 @@ export const dbConnect = async () => {
     console.log(`Error de conexión a la base de datos: ${error.message}`);
   }
 };
-
-const conn = mongoose.connection;
-
-conn.on('error', () => console.error.bind(console, 'connection error'));
-
-conn.once('open', () => console.info('Connection to Database is successful'));
-
-export default conn;
-/* export default dbConnect; */
