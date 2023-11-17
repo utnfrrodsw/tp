eb8a22e0e43cb6eecedcb8e393cae0085dcb3b80
andres guerrero <andresguerreroutn@gmail.com>
2023-11-16 21:08:08 -0300

    list with pagination & axios

--- app/src/views/ListTechnicians.vue
+++ app/src/views/ListTechnicians.vue
@@ -1,22 +1,41 @@
 <template>
   <v-container fluid>
-    <v-row >
-      <v-col v-for="(item, index) in items" :key="index" cols="3">
-        <v-card outlined>
-          <v-card-title>{{ item.name }}</v-card-title>
-          <v-card-text>
-            <v-row>
-              <v-col cols="6">Codigo:</v-col>
-              <v-col cols="6">{{ item.id }}</v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="6">Fecha de Nacimiento:</v-col>
-              <v-col cols="6">{{ item.date_born.substring(0,10) }}</v-col>
-            </v-row>
-          </v-card-text>
-          <v-card-actions>
-            <v-btn color="primary" @click="editTechnician(item.id)">Modificar</v-btn>
-          </v-card-actions>
+    <v-row align="center" class="list px-3 mx-auto">
+      <v-col cols="12" sm="8">
+        <v-text-field v-model="searchName" label="Buscar por nombre"></v-text-field>
+      </v-col>
+
+      <v-col cols="12" sm="4">
+        <v-btn small @click="page = 1; retrieveTechnician();">
+          Search
+        </v-btn>
+      </v-col>
+
+      <v-col cols="4" sm="3">
+        <v-select v-model="pageSize" :items="pageSizes" label="Items por pagina" @change="handlePageSizeChange"></v-select>
+      </v-col>
+
+      <v-col cols="12" sm="9">
+        <v-pagination 
+          v-model="page" 
+          :length="totalPages" 
+          total-visible="7" 
+          next-icon="mdi-menu-right"
+          prev-icon="mdi-menu-left" 
+          @input="handlePageChange">
+        </v-pagination>
+      </v-col>
+
+      <v-col cols="12" sm="12">
+        <v-card class="mx-auto" tile>
+          <v-card-title>Tecnicos</v-card-title>
+          <v-data-table :headers="headers" :items="items" disable-pagination :hide-default-footer="true">
+            <template v-slot:[`item.actions`]="{ item }">
+              <v-icon small class="mr-2" @click="editTechnician(item.id)">
+                mdi-pencil
+              </v-icon>
+            </template>
+          </v-data-table>
         </v-card>
       </v-col>
     </v-row>
@@ -24,25 +43,79 @@
 </template>
 
 <script>
-  export default {
-    name: 'ListTechnicians',
-    data() {
-      return {
-        items: []
-      }
-    },
-    mounted() {
-      this.fetchData()
-    },
-    methods: {
-      async fetchData() {
-        const response = await fetch(`${process.env.VUE_APP_API_URL}api/technicians`)
-        const data = await response.json()
-        this.items = data
-      },
-      editTechnician(id) {
-        this.$router.push({ name: 'EditTechnician', params: { id } })
-      }
+import TechnicianDataService from "../services/TechnicianDataService";
+export default {
+  name: 'ListTechnicians',
+  data() {
+    return {
+      items: [],
+      page: 1,
+      totalPages: 0,
+      pageSize: 3,
+      pageSizes: [3, 6, 9],
+      searchName: "",
+      headers: [
+        { text: "Codigo", align: "start", sortable: false, value: "id" },
+        { text: "Nombre", value: "name", sortable: false },
+        { text: "Fecha nacimiento", value: "date_born", sortable: false },
+        { text: "Opciones", value: "actions", sortable: false },
+      ],
     }
+  },
+  mounted() {
+    this.retrieveTechnician();
+  },
+  methods: {
+    getRequestParams(page, pageSize) {
+      let params = {};
+
+      if (page) {
+        params["page"] = page - 1;
+      }
+
+      if (pageSize) {
+        params["size"] = pageSize;
+      }
+
+      return params;
+    },
+    retrieveTechnician() {
+      const params = this.getRequestParams(
+        this.page,
+        this.pageSize
+      );
+
+      TechnicianDataService.getAll(params)
+        .then((response) => {
+          const { technicians, totalPages } = response.data;
+          this.items = technicians.map(this.getDisplayTechnician);
+          this.totalPages = totalPages;
+
+          console.log(response.data);
+        })
+        .catch((e) => {
+          console.log(e);
+        });
+    },
+    editTechnician(id) {
+      this.$router.push({ name: 'EditTechnician', params: { id } })
+    },
+    handlePageChange(value) {
+      this.page = value;
+      this.retrieveTechnician();
+    },
+    handlePageSizeChange(size) {
+      this.pageSize = size;
+      this.page = 1;
+      this.retrieveTechnician();
+    },
+    getDisplayTechnician(technician) {
+      return {
+        id: technician.id,
+        name: technician.name,
+        date_born: new Date(technician.date_born).toLocaleDateString().substring(0,10)
+      };
+    },
   }
+}
 </script>
