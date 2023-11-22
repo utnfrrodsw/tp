34f568e30de31e2b3256a5e271e42d4d464209a5
Gaston Gimenez <gaston.ignacio.gimenez@gmail.com>
2023-11-22 10:23:12 -0300

    [FIX] New login view and Login responsive view

--- app/src/views/Login.vue
+++ app/src/views/Login.vue
@@ -1,9 +1,11 @@
 <template>
   <v-container fluid fill-height>
     <v-layout align-center justify-center>
-      <v-flex xs12 sm6 md4>
-        <v-card class="elevation-12">
-          <v-card-title class="headline text-center">Iniciar Sesión</v-card-title>
+      <v-flex xs12 sm8 md6>
+        <v-card class="custom-card" elevation="12">
+          <v-card-title class="headline text-center">
+            Iniciar Sesión
+          </v-card-title>
           <v-card-text>
             <v-form @submit.prevent="login">
               <v-text-field
@@ -11,6 +13,7 @@
                 label="Email"
                 outlined
                 solo
+                prepend-icon="mdi-account"
               ></v-text-field>
               <v-text-field
                 v-model="password"
@@ -18,6 +21,7 @@
                 type="password"
                 outlined
                 solo
+                prepend-icon="mdi-lock"
               ></v-text-field>
               <v-btn block type="submit" color="primary">Iniciar Sesión</v-btn>
             </v-form>
@@ -29,39 +33,47 @@
 </template>
 
 <script>
-  import axios from 'axios'
+import axios from 'axios'
 
-  export default {
-    data() {
-      return {
-        email: '',
-        password: ''
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
+  methods: {
+    async login() {
+      try {
+        const body = {
+          email: this.email,
+          password: this.password,
+        };
+        const redirectPath = this.$route.query.redirect || '/'
+        const apiUrl = process.env.VUE_APP_API_URL
+        const url = `${apiUrl}api/auth/login`
+        const response = await axios.post(url, body)
+        const data = await response.data
+        localStorage.setItem('token', data.token)
+        this.$router.push(redirectPath)
+      } catch (error) {
+        console.error(error)
       }
     },
-    methods: {
-      async login() {
-        try {
-          const body = {
-            email: this.email,
-            password: this.password
-          }
-          const redirectPath = this.$route.query.redirect || '/'
-          const apiUrl = process.env.VUE_APP_API_URL
-          const url = `${apiUrl}api/auth/login`
-          const response = await axios.post(url, body)
-          const data = await response.data
-          localStorage.setItem('token', data.token)
-          this.$router.push(redirectPath)
-        } catch (error) {
-          console.log(error)
-        }
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style scoped>
-  .elevation-12 {
-    box-shadow: 0 6px 10px rgba(0, 0, 0, 0.14), 0 1px 18px rgba(0, 0, 0, 0.12), 0 3px 5px rgba(0, 0, 0, 0.2);
-  }
+.custom-card {
+  border-radius: 12px; 
+  overflow: hidden; 
+  border: 1px solid lightgrey; 
+  max-width: 400px; 
+  margin: auto; 
+}
+
+.elevation-12 {
+  box-shadow: 0 6px 10px rgba(0, 0, 0, 0.14), 0 1px 18px rgba(0, 0, 0, 0.12), 0 3px 5px rgba(0, 0, 0, 0.2);
+}
 </style>
