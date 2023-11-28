52a2d54cc6b10b3b69bce66fcb39a649e63f1702
Aguobiedo <agus1000obiedo@gmail.com>
2023-11-28 14:37:08 -0300

    Update Login.jsx

--- client/src/components/usuario/login/Login.jsx
+++ client/src/components/usuario/login/Login.jsx
@@ -1,58 +1,53 @@
 import React, { useState } from 'react';
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../../auth/authProvider';
+import { loginUser } from '../../../services/Login';
 import './Login.css';
 const { API_URL } = require('../../../auth/constants');
 
-function Login () {
+function Login() {
   const [email, setEmail] = useState('');
-  const [constrasena, setContrasena] = useState('');
-  const [errorResponse, setErrorResponse] = useState(null); // Estado para mensajes de error
+  const [constrasena, setContrasena] = useState(''); // Corregido el nombre de la variable
+  const [errorResponse, setErrorResponse] = useState(null);
   const goTo = useNavigate();
   const auth = useAuth();
   const [mostrarContrasena, setMostrarContrasena] = useState(false);
 
-
   async function handleLogin(e) {
     e.preventDefault();
     try {
-      const response = await fetch(`${API_URL}/usuario/login`, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          email,
-          constrasena,
-        }),
-      });
-
-      const json = await response.json();
+      const { response, json, error } = await loginUser(email, constrasena); 
 
       if (response.ok) {
         setErrorResponse(null);
-        
 
-        if (json.body.token && json.body.refreshToken) {
+        const responseBody = json.body;
+
+        if (responseBody.token && responseBody.refreshToken) {
           auth.saveUser(json);
-          if (json.body.user.esPrestador) {
+
+          if (responseBody.user.esPrestador) {
             goTo('/provider/home');
-          } else if (!json.body.user.esPrestador) {
+          } else if (!responseBody.user.esPrestador) {
             goTo('/client/home');
           }
         }
       } else {
+        setErrorResponse(
+          error ||
+            (json && json.errors && json.errors.length > 0
+              ? json.errors[0].msg
+              : json.message)
+        );
 
-        // Si la respuesta contiene errores de validación, manejarlos aquí
-        setErrorResponse(json.errors && json.errors.length > 0 ? json.errors[0].msg : json.message );
-
-        if (json.statusCode === 401) {
+        if (json && json.statusCode === 401) {
           setErrorResponse('Usuario o contraseña incorrectos');
         }
       }
     } catch (error) {
-
-      setErrorResponse(error.message || 'Error al conectar con el servidor');
+      setErrorResponse(
+        error.message || 'Error al conectar con el servidor'
+      );
     }
   }
 
@@ -74,20 +69,20 @@ function Login () {
             <i className='bx bxs-user'></i>
           </div>
           <div className="input-box">
-          <input
-             type={mostrarContrasena ? "text" : "password"}
-             placeholder="Contraseña"
-             value={constrasena}
-             onChange={(e) => setContrasena(e.target.value)}
-           />
-           <button
+            <input
+              type={mostrarContrasena ? "text" : "password"}
+              placeholder="Contraseña"
+              value={constrasena} // Corregido el nombre de la variable
+              onChange={(e) => setContrasena(e.target.value)} // Corregido el nombre de la variable
+            />
+            <button
               type="button"
               className="mostrar-ocultar"
               onClick={() => setMostrarContrasena(!mostrarContrasena)}
-              >
-            {mostrarContrasena ? "🙈" : "👁️"}
-           </button>
-           </div>
+            >
+              {mostrarContrasena ? "🙈" : "👁️"}
+            </button>
+          </div>
           {!!errorResponse && (
             <div className="error-message">{errorResponse}</div>
           )}
@@ -95,8 +90,10 @@ function Login () {
             <label><input type="checkbox" /> <p>Recuérdame</p> </label>
             <Link to="/recuperarClave">Recuperar Contraseña</Link>
           </div>
-          <button type="submit" className="btn">Login</button>
-          
+          <button type="submit" className="btn">
+            Login
+          </button>
+
           <div className="register-link">
             <p>No tienes cuenta? <Link to="/register">Regístrate</Link></p>
           </div>
@@ -104,6 +101,6 @@ function Login () {
       </div>
     </section>
   );
-};
+}
 
 export default Login;
