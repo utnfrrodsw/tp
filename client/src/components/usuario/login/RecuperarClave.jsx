c4c8df2a6ee692678608c77ad17a8e89f979da1d
Aguobiedo <agus1000obiedo@gmail.com>
2023-12-02 00:08:28 -0300

    Update RecuperarClave.jsx

--- client/src/components/usuario/login/RecuperarClave.jsx
+++ client/src/components/usuario/login/RecuperarClave.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import recuperarClave from '../../../services/RecuperarClave';
+import { recClave } from '../../../services/RecuperarClave.js';
+import Loader from '../../load/loader/Loader';
 import './RecuperarClave.css';
-const { API_URL } = require('../../../auth/constants');
 
 const PasswordResetPage = () => {
   // Inicializamos la función de navegación.
@@ -16,21 +16,24 @@ const PasswordResetPage = () => {
   // Agregamos una variable de estado para almacenar el estado de respuesta.
   const [responseStatus, setResponseStatus] = useState(null);
 
+  // Estado para controlar el loader
+  const [isLoading, setIsLoading] = useState(false);
+
   // Función para actualizar el estado 'email' cuando el usuario escribe en el campo de entrada.
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
   };
 
- 
   // Función para manejar la solicitud de restablecimiento de contraseña.
   const handlePasswordReset = async () => {
+    setIsLoading(true);
+
     try {
-      const { response, json, error } = await recuperarClave('/usuario/reset-password/request', 'POST', { email });
-
+      const jsonResponse = await recClave(email);
       // Actualizamos el estado de respuesta.
-      setResponseStatus(response.status);
-
-      if (response.status === 200) {
+      setResponseStatus(jsonResponse.statusCode);
+       
+      if (jsonResponse.statusCode === 200) {
         setMessage('Se ha enviado un código a tu correo electrónico, por favor revisa tu bandeja de entrada');
         setSubmitted(true);
         const loginURL = `/reset-password?email=${email}`;
@@ -39,16 +42,21 @@ const PasswordResetPage = () => {
           goTo(loginURL);
         }, 5000);
       } else {
-        // Si la respuesta contiene errores de validación, manejarlos aquí
-        setMessage(json.errors && json.errors.length > 0 ? json.errors[0].msg : json.message);
-       
-        if (json.statusCode === 404) {
+        const firstError = jsonResponse.error || (jsonResponse.errors && jsonResponse.errors.length > 0
+          ? jsonResponse.errors[0].msg
+          : 'Error durante el restablecimiento de la contraseña');
+
+        setMessage(firstError);
+
+        if (jsonResponse.statusCode === 404) {
           setMessage('No se encontró una cuenta con ese correo electrónico');
         }
       }
     } catch (error) {
       console.error('Error al solicitar el restablecimiento de contraseña:', error);
       setMessage('Error al solicitar el restablecimiento de contraseña');
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -59,26 +67,30 @@ const PasswordResetPage = () => {
   };
 
   return (
-    <div className="conteinerRecClave">
-      <h2>Recuperación de Contraseña</h2>
-      {message && <div className={`message ${responseStatus === 200 ? 'success' : 'error'}`}>{message}</div>}
-      {!submitted && (
-        <form onSubmit={handleSubmit}>
-          <div className="Formulario">
-            <label htmlFor="email">Proporciona tu email de registro</label>
-            <input
-              id="email"
-              name="email"
-              value={email}
-              onChange={handleEmailChange}
-            />
-          </div>
-          <div className="formularioGrupo">
-            <button className='enviarSolicitud' type="submit">Enviar Solicitud</button>
-          </div>
-        </form>
+    <section>
+      {isLoading ? <Loader /> : (
+        <div className="conteinerRecClave">
+          <h2>Recuperación de Contraseña</h2>
+          {message && <div className={`message ${responseStatus === 200 ? 'success' : 'error'}`}>{message}</div>}
+          {!submitted && (
+            <form onSubmit={handleSubmit}>
+              <div className="Formulario">
+                <label htmlFor="email">Proporciona tu email de registro</label>
+                <input
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={handleEmailChange}
+                />
+              </div>
+              <div className="formularioGrupo">
+                <button className='enviarSolicitud' type="submit">Enviar Solicitud</button>
+              </div>
+            </form>
+          )}
+        </div>
       )}
-    </div>
+    </section>
   );
 };
 
