6efbeac5f584d896ed4960bdbc5281ffc5b2ebbc
Franco <francogimenez981@gmail.com>
2024-04-07 13:09:50 -0300

    Agregue un componente

--- frontend/hotel/src/app/app.module.ts
+++ frontend/hotel/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
---
+++ frontend/hotel/src/app/inicio/inicio.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { InicioComponent } from './inicio.component';
+
+describe('InicioComponent', () => {
+  let component: InicioComponent;
+  let fixture: ComponentFixture<InicioComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [InicioComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(InicioComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
