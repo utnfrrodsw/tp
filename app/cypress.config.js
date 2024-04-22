8bc7dae645aae87b15be89c5ad5bd77c5b11612b
Martinalbertengo <tincho9704@gmail.com>
2024-04-22 12:01:01 -0300

    cypress installed

---
+++ app/cypress.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: "http://localhost:8080",
+    "fixturesFolder": "./api/mocks"
+  },
+
+  component: {
+    devServer: {
+      framework: "vue-cli",
+      bundler: "webpack"
+    }
+  }
+})
