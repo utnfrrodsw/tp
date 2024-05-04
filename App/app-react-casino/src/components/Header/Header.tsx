b7cb6c496d6d696feff72aeb4c09b1bd25e88ab6
Lucas <lucasmaggi03@gmail.com>
2024-05-04 19:58:15 -0300

    Update header

--- App/app-react-casino/src/components/Header/Header.tsx
+++ App/app-react-casino/src/components/Header/Header.tsx
@@ -3,10 +3,22 @@ import './Header.css'
 
 export function Header() {
     return(
-        <div className='header'>
-                <Link to="/"><img src="./src/assets/images/boceto_logo.png" /></Link>
-                <Link to="/chau">Principal</Link>
-                <Link to="/hola">Principal</Link>
-        </div>
+        <header className=''>
+            <div className='w-screen flex'>
+                <video muted autoPlay loop playsInline className='w-screen abosulute'>
+                    <source src='./src/assets/vid-bg/vid-bg-header.mp4'/>
+                </video>
+                <nav className='bg-[#fabc01]/10 py-6 absolute w-screen'>
+                    <div className='container mx-auto flex items-center justify-between'>
+                            <Link to="/"><img className='max-h-10' src="./src/assets/images/boceto_logo.png" /></Link>
+                            <div className='place-items-end space-x-10'>
+                                <Link to="/chau">About Us</Link>
+                                <Link to="/chau">Daily Free</Link>
+                                <Link to="/hola" className='border border-white py-2 px-5'>LogIn</Link>
+                            </div>
+                    </div>
+                </nav>
+            </div>
+        </header>
     )
 }
--- App/app-react-casino/src/pages/Principal/Principal.tsx
+++ App/app-react-casino/src/pages/Principal/Principal.tsx
@@ -2,6 +2,6 @@ import './Principal.css'
 
 export function Principal() {
     return(
-            <p className='lahsdkjahwlkjda'>Hello World</p>
+            <p className=''></p>
     )
 }
