c8f1e4adec8a46ed3fa88b63d995c872f308da95
Lucas <lucasmaggi03@gmail.com>
2024-05-04 15:42:41 -0300

    main

--- App/app-react-casino/src/main.tsx
+++ App/app-react-casino/src/main.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <Pagina Principal/>,
+  errorElement: <Error />,
+},
+{
+  path: '/login',
+  element: <Login/>,
+}
+]);
+<RouterProvider router={router} />
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     
   </React.StrictMode>,
 )
+
