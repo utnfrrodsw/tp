ecfcc986ac91cbe5aa0b816184a3eb20ed9c6118
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-05-07 16:05:16 -0300

    Update Principal.tsx

--- App/app-react-casino/src/pages/Principal/Principal.tsx
+++ App/app-react-casino/src/pages/Principal/Principal.tsx
@@ -4,7 +4,7 @@ export function Principal() {
     return(
         <div>
             <section className='prueba'>
-                <video muted autoPlay loop className='top-0 absolute -z-10 prueba2'> {/* use 2 clases de CSS, "prueba" y "prueba2", nose usar Tailwind y queria arreglarlo rapido, despues veo de pasarlas a tailwind */}
+                <video muted autoPlay loop disablePictureInPicture className='top-0 absolute -z-10 prueba2'> {/* use 2 clases de CSS, "prueba" y "prueba2", nose usar Tailwind y queria arreglarlo rapido, despues veo de pasarlas a tailwind */}
                     <source src='./src/assets/vid-bg/vid-bg-header.mp4'/>
                 </video>
                 {/* Se armo medio un quilombito que tuve que corregir con el tema de la posicion absoluta, Todavia no quedo completamente arreglado pero creo que quedo "decente" y era que el video seguia el scroll vertical y siempre quedaba pegado a la pantalla (por algun motivo), queda perfeccionarlo*/}
