11896b178214225b4628b7ca6c40200db163c842
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-05-07 14:29:01 -0300

    Arreglos y cambio de titulo

--- App/app-react-casino/src/components/Header/Header.tsx
+++ App/app-react-casino/src/components/Header/Header.tsx
@@ -4,16 +4,14 @@ import './Header.css'
 export function Header() {
     return(
         <header className='sticky top-0 z-10'>
-                <nav className='bg-[#fabc01]/10 py-6 w-full'>
-                    <div className='mx-10 flex items-center justify-between'>
-                            <Link to="/"><img className='max-h-10' src="./src/assets/images/boceto_logo.png" /></Link>
-                            <div className='place-items-end space-x-10'>
-                                <Link to="/chau">About Us</Link>
-                                <Link to="/chau">Daily Free</Link>
-                                <Link to="/hola" className='border border-white py-2 px-5'>LogIn</Link>
-                            </div>
-                    </div>
-                </nav>
+            <nav className='bg-[#fabc01]/10 p-6 w-full flex items-center justify-between'>
+                <Link to="/"><img className='max-h-12' src="./src/assets/images/boceto_logo.png" /></Link>
+                <div className='place-items-end space-x-10'>
+                    <Link to="/chau">About Us</Link>
+                    <Link to="/chau">Daily Free</Link>
+                    <Link to="/hola" className='border border-white py-2 px-5'>LogIn</Link>
+                </div>
+            </nav>
         </header>
     )
 }
