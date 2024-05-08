35ac685ff7673b92696405646b8569b31420a45a
Agus Gil <agussv360@gmail.com>
2024-05-08 13:32:33 -0300

    Update Texts
    Update Casino Text
    Update UTimbaN Text

--- App/app-react-casino/src/pages/Principal/Principal.tsx
+++ App/app-react-casino/src/pages/Principal/Principal.tsx
@@ -9,12 +9,12 @@ export function Principal() {
                 </video>
                 {/* Se armo medio un quilombito que tuve que corregir con el tema de la posicion absoluta, Todavia no quedo completamente arreglado pero creo que quedo "decente" y era que el video seguia el scroll vertical y siempre quedaba pegado a la pantalla (por algun motivo), queda perfeccionarlo*/}
                 <div className='imagenes'> {/* Se puede mejorar, despues veo como lo hago. */}
-                    <img src="./src/assets/images/CASINO.png" alt="Casino"/>
-                    <img src="./src/assets/images/UTIMBAN.png" alt="UtimbaN"/>
+                    <img src="./src/assets/images/casino.gif" alt="Casino"/>
+                    <img src="./src/assets/images/utimban.gif" alt="UtimbaN"/>
                 </div>
             </section>
             <section className='px-20'>
-                {/*QUIZAS ES MEJOR PONER LOS ESTILOS EN UN CSS*/}
+                {/*QUIZAS ES MEJOR PONER LOS ESTILOS EN UN CSS ??? que decis mauri*/}
                 <h1 className='pb-10 text-5xl text-[#70ae6e]'>Games</h1>
                 <div className='grid grid-cols-4 gap-10 pb-20 rounded-xl'>
                     <div className='bg-red-500 border-2 text-white rounded-[50px]'></div>
@@ -41,5 +41,6 @@ export function Principal() {
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, quis illum suscipit molestias architecto inventore exercitationem rerum, nulla, perferendis modi cumque iusto voluptas excepturi distinctio? Laborum quaerat placeat consectetur ducimus!</p>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quasi, quis illum suscipit molestias architecto inventore exercitationem rerum, nulla, perferendis modi cumque iusto voluptas excepturi distinctio? Laborum quaerat placeat consectetur ducimus!</p>
         </div>
+    
     )
 }
