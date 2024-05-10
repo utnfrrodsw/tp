d62865e03899d48e47ac07cf004ff4072939aea0
Lotar17 <102708361+Lotar17@users.noreply.github.com>
2024-05-10 20:08:41 -0300

    Terminar crud sin validaciones

--- primerCrud.js
+++ primerCrud.js
@@ -2,35 +2,85 @@
 
 import * as readline from 'node:readline/promises';
 import { stdin as input, stdout as output } from 'node:process';
-import { error } from 'node:console';
+import { Persona } from './clases.js';
 
 const personas = [];
-
 const rl = readline.createInterface({ input, output });
-export class Persona {
-  constructor(dni, nombre, apellido, telefono, mail) {
-    this.dni = dni;
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.telefono = telefono;
-    this.mail = mail;
-  }
+main();
+
+async function main() {
+  let opcion;
+  do {
+    console.log(
+      '\n1-CREAR PERSONA\n2-BUSCAR PERSONA\n3-MODIFICAR PERSONA\n4-BORRAR PERSONA\n5-VER TODOS\n6-SALIR'
+    );
+    opcion = Number.parseInt(
+      await rl.question('\nIngrese la opción a la que quiere acceder: \n')
+    );
+    switch (opcion) {
+      case 1:
+        await ingresarPersona();
+        break;
+      case 2:
+        if (personasEstaVacio()) {
+          console.log(
+            '\nNo se puede buscar ninguna persona porque no hay ninguna persona cargada aún\n'
+          );
+        } else {
+          await buscarPersona();
+        }
+        break;
+      case 3:
+        if (personasEstaVacio()) {
+          console.log(
+            '\nNo se puede modificar ninguna persona porque no hay ninguna persona cargada aún\n'
+          );
+        } else {
+          await modificarPersona();
+        }
+        break;
+      case 4:
+        if (personasEstaVacio()) {
+          console.log(
+            '\nNo se puede borrar ninguna persona porque no hay ninguna persona cargada aún\n'
+          );
+        } else {
+          await borrarPersona();
+        }
+        break;
+      case 5:
+        if (personasEstaVacio()) {
+          console.log('\nAún no se ha cargado ninguna persona');
+        } else {
+          verTodos();
+        }
+
+        break;
+    }
+  } while (opcion !== 6);
+  rl.close();
 }
+
 async function ingresarPersona() {
   try {
-    const dni = await rl.question('Ingrese el dni: \n');
-    const nombre = await rl.question('Ingrese el nombre: \n');
-    const apellido = await rl.question('Ingrese el apellido: \n');
-    const telefono = await rl.question('Ingrese el telefono: \n');
-    const mail = await rl.question('Ingrese el mail: \n');
-
-    const persona = new Persona(dni, nombre, apellido, telefono, mail);
+    const persona = await crearPersona();
     console.log(persona);
     personas.push(persona);
   } catch (error) {
     console.log('Error al ingresar la persona:', error);
   }
 }
+
+const crearPersona = async () => {
+  const dni = await rl.question('Ingrese el dni: \n');
+  const nombre = await rl.question('Ingrese el nombre: \n');
+  const apellido = await rl.question('Ingrese el apellido: \n');
+  const telefono = await rl.question('Ingrese el telefono: \n');
+  const mail = await rl.question('Ingrese el mail: \n');
+  const persona = new Persona(dni, nombre, apellido, telefono, mail);
+  return persona;
+};
+
 async function buscarPersona() {
   try {
     const dnibuscado = await rl.question(
@@ -55,31 +105,42 @@ const verTodos = () => {
   console.log(JSON.stringify(personas, null, 2));
 };
 
-async function main() {
-  let opcion;
-  do {
-    console.log(
-      '1-CREAR PERSONA\n2-BUSCAR PERSONA\n3-MODIFICAR PERSONA\n4-BORRAR PERSONA\n5-VER TODOS\n6-SALIR'
+const modificarPersona = async () => {
+  try {
+    const dnibuscado = await rl.question(
+      'Ingrese el dni de la persona que desea buscar:\n'
     );
-    opcion = Number.parseInt(
-      await rl.question('\nIngrese la opción a la que quiere acceder: \n')
-    );
-    switch (opcion) {
-      case 1:
-        await ingresarPersona();
-        break;
-      case 2:
-        await buscarPersona();
-        break;
-      case 3:
-        break;
-      case 4:
-        break;
-      case 5:
-        verTodos();
-        break;
+    const persona = buscodni(personas, dnibuscado);
+    const indice = personas.findIndex((element) => element.dni === persona.dni);
+    console.log(`Persona encontrada: ${JSON.stringify(persona)}`);
+    const personaReemplazo = await crearPersona();
+    personas.splice(indice, 1, personaReemplazo);
+  } catch (error) {
+    if (error instanceof TypeError) {
+      console.log('\nLa persona ingresada no existe\n');
+    } else {
+      console.log('Error al modificar la persona: ', error);
     }
-  } while (opcion !== 6);
-  rl.close();
-}
-main();
+  }
+};
+
+const personasEstaVacio = () => {
+  return personas.length === 0 ? true : false;
+};
+
+const borrarPersona = async () => {
+  try {
+    const dnibuscado = await rl.question(
+      'Ingrese el dni de la persona que desea borrar:\n'
+    );
+    const indice = personas.findIndex((element) => element.dni === dnibuscado);
+    if (indice === -1) {
+      console.log('\nEl dni ingresado no existe\n');
+    } else {
+      personas.splice(indice, 1);
+      console.log('\nPersona borrada con éxito');
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
