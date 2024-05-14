1a88a1079d3c91a517ddf5085a17d3f21d7a422c
Nicolás Gabriel Gallegos <ngallegos@frro.utn.edu.ar>
2024-05-14 18:53:54 -0300

    Login Update
    El Login ahora se cierra correctamente cuando presionas afuera del cuadro

--- App/app-react-casino/src/App.tsx
+++ App/app-react-casino/src/App.tsx
@@ -4,7 +4,6 @@ import { Header } from './components/Header/Header.tsx'
 import { Footer } from './components/Footer/Footer.tsx'
 import { Dice } from './components/Games/Dice/Dice.tsx'
 import './App.css'
-import { LogIn } from './components/LogIn/LogIn.tsx'
 
 export function App() {
     return(
