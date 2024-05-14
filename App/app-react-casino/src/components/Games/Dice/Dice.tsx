35e089f9d81ff1067a471d2981aee415f5f37f17
Lucas <lucasmaggi03@gmail.com>
2024-05-14 17:08:31 -0300

    css dice

--- App/app-react-casino/src/components/Games/Dice/Dice.tsx
+++ App/app-react-casino/src/components/Games/Dice/Dice.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import './Dice.css'
 export function Dice(){
     const[data,setData]=useState(0)
     return(
@@ -8,14 +8,14 @@ export function Dice(){
                 <div className="col-span-1 row-span-2 bg-[color:var(--violeta)] w-full h-full p-2">
                     <div className="pb-5">
                         <label className="">Monto de Apuesta</label>
-                        <input className="text-black rounded-[20px] p-2 w-[80%]" inputMode="decimal" placeholder="0.00" type="number"  min="0" />
+                        <input step="0.01" className="bg-[color:var(--blanco)] text-black rounded-[20px] p-2 w-[80%]" inputMode="decimal" placeholder="0.00" type="number"  min="0" />
                     </div>
                         <label className="">Recibe al Ganar</label>
                         <input className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled />
                     <button className="bg-[color:var(--amarillo)] text-[color:var(--negro)] py-3 w-[80%] mt-[20px] text-bold">APOSTAR</button>
                 </div>
                 <div className="col-span-2 text-center w-full">
-                        <input className="w-[90%] " type="range" min="2" max="98" value={data} step="1" onChange={(e)=>setData(e.target.value)}/>
+                        <input className="w-[90%] slider" type="range" min="2" max="98" value={data} step="1" onChange={(e)=>setData(e.target.value)}/>
                         <h1 id="demo">{data}</h1>
                 </div>
                 <div className="col-span-2 flex justify-between w-[90%] bg-[color:var(--violeta)] p-5">
@@ -24,7 +24,7 @@ export function Dice(){
                         <input className="bg-[color:var(--negro)]" type="number" step="0.01" min="1.0102" max="9990" value={(1/data)*100}/>
                     </div>
                     <div>
-                        <label><h1>Menor</h1>r</label>
+                        <label><h1>Menor</h1></label>
                         <input className="bg-[color:var(--negro)]" type="number" min="1.0102" max="9990" value={data} />
                     </div>
                     <div>
