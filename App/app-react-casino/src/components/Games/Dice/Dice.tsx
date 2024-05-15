350a3008834fa211a7060ccae18a82a9583cf2de
Lucas <lucasmaggi03@gmail.com>
2024-05-15 00:05:16 -0300

    style dice

--- App/app-react-casino/src/components/Games/Dice/Dice.tsx
+++ App/app-react-casino/src/components/Games/Dice/Dice.tsx
@@ -32,7 +32,16 @@ export function Dice() {
         setData(data);
         calcularRecibeAlGanar(data, monto);
         calcularMulti(data);
-    
+        setSliderColor();
+    }
+
+    const setSliderColor = () => {
+        const slider = document.querySelector('.slider');
+        const thumb = document.querySelector('.slider-thumb');
+        const percent = data;
+        const color = `linear-gradient(90deg, var(--rojo) ${percent}%, var(--blanco) ${percent}%)`;
+        slider.style.background = color;
+        thumb.style.left = `calc(${percent}% - 12px)`;
     }
 
     const generarNumero = () => {
@@ -47,32 +56,30 @@ export function Dice() {
     }
     return (
         <>
-            <section className="place-items-center border-[color:var(--violeta)] border-[20px] rounded-[30px] mx-[200px] my-[100px] h-[500px] gap-0 grid grid-cols-3 grid-rows-2">
-                <div className="col-span-1 row-span-2 bg-[color:var(--violeta)] w-full h-full p-2">
-                    <div className="pb-5">
-                        <label className="">Monto de Apuesta</label>
-                        <input step="0.01" className="bg-[color:var(--blanco)] text-black rounded-[20px] p-2 w-[80%]" min="0" value={monto} onChange={montoApuesta}/>
-                    </div>
+            <section className="place-items-center border-[color:var(--violeta)] border-[20px] rounded-[30px] mx-[200px] mt-[150px] mb-[50px] h-[500px] gap-0 grid grid-cols-3 grid-rows-2 max-lg:mx-[20px] max-lg:grid-cols-1">
+                <div className="col-span-1 row-span-2 bg-[color:var(--violeta)] w-full h-full p-2 flex flex-col justify-center items-center">
+                    <label className="">Monto de Apuesta</label>
+                    <input step="0.01" className="bg-[color:var(--blanco)] text-black rounded-[20px] p-2 w-[80%] mb-2" min="0" value={monto} onChange={montoApuesta}/>
                     <label className="">Recibe al Ganar</label>
                     <input className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" value={recibeAlGanar} disabled/>
-                    <button onClick={generarNumero} className="bg-[color:var(--amarillo)] text-[color:var(--negro)] py-3 w-[80%] mt-[20px] text-bold">APOSTAR</button>
+                    <button onClick={generarNumero} className="bg-[color:var(--amarillo)] hover:bg-yellow-600 text-[color:var(--negro)] py-3 w-[80%] mt-[20px] text-bold max-lg:mb-[20px]">APOSTAR</button>
                 </div>
-                <div className="col-span-2 text-center w-full">
+                <div className="col-span-2 text-center w-full mt-20">
                     <input className="w-[90%] slider" type="range" min="2" max="98" value={data} step="1" onChange={sliderData}/>
-                    <h1 id="demo">{data}</h1>
+                    <h1 className="text-[30px] font-bold" id="demo">{data}</h1>
                 </div>
-                <div className="col-span-2 flex justify-between w-[90%] bg-[color:var(--violeta)] p-5">
+                <div className="col-span-2 flex justify-center gap-10 flex-row w-[90%] bg-[color:var(--violeta)] p-5 max-lg:block max-lg:justify-center max-lg:w-full max-lg:flex-col max-lg:justify-center max-lg:items-center ">
                     <div>
                         <label><h1>Multiplicador</h1></label>
-                        <input className="bg-[color:var(--negro)]" type="number" step="0.01" min="1.0102" max="9990" value={multi} onChange={calcularMulti}/>
+                        <input className="bg-[color:var(--negro)] bg-[color:var(--negro)] text-[color:var(--blanco)] rounded-[20px] p-2 w-[80%]" type="number" step="0.01" min="1.0102" max="9990" value={multi} onChange={calcularMulti}/>
                     </div>
                     <div>
                         <label><h1>Menor</h1></label>
-                        <input className="bg-[color:var(--negro)]" type="number" min="1.0102" max="9990" value={data} />
+                        <input className="bg-[color:var(--negro)] text-[color:var(--blanco)] rounded-[20px] p-2 w-[80%]" type="number" min="1.0102" max="9990" value={data} />
                     </div>
                     <div>
                         <label><h1>Probabilidad</h1></label>
-                        <input className="bg-[color:var(--negro)]" type="number" min="1.0102" max="9990" value={data} />
+                        <input className="bg-[color:var(--negro)] text-[color:var(--blanco)] rounded-[20px] p-2 w-[80%]" type="number" min="1.0102" max="9990" value={data} />
                     </div>
                 </div>
             </section>
