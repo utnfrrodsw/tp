e0da026e1db1d03ef61a7afa3b0ba47cb4d7c3c1
Lucas <lucasmaggi03@gmail.com>
2024-05-16 13:47:47 -0300

    update principal & dice

--- App/app-react-casino/src/components/Games/Dice/Dice.tsx
+++ App/app-react-casino/src/components/Games/Dice/Dice.tsx
@@ -10,14 +10,20 @@ export function Dice() {
     const [monto, setMonto] = useState(0.0);
     const [multi, setMulti] = useState(0.0);
     const [recibeAlGanar, setRecibeAlGanar] = useState(0.0);
+    const [mayorMenor, setMayorMenor] = useState(true);
 
     const calcularMulti = (data:number)=>{
-        const multi = ((1/data)*100).toFixed(2);
-        setMulti(multi)
+        if(textoBoton == "Menor"){
+            const multi = ((1/(data))*100).toFixed(2);
+            setMulti(multi)
+        }else{
+            const multi = ((1/(100-data))*100).toFixed(2);
+            setMulti(multi)
+        }
     }
 
     const calcularRecibeAlGanar = (data:number, monto:number) => {
-        const recibeAlGanar = (((1 / data) * 100) * monto).toFixed(2);
+        const recibeAlGanar = (multi * monto).toFixed(2);
         setRecibeAlGanar(recibeAlGanar);
     }
 
@@ -28,8 +34,13 @@ export function Dice() {
     }
     
     const sliderData = (event) => {
-        const data = parseInt(event.target.value);
-        setData(data);
+        if(textoBoton == "Menor"){
+            const data = parseInt(event.target.value);
+            setData(data);
+        }else{
+            const data = 100 - parseInt(event.target.value);
+            setData(data);
+        }
         calcularRecibeAlGanar(data, monto);
         calcularMulti(data);
         setSliderColor();
@@ -39,9 +50,15 @@ export function Dice() {
         const slider = document.querySelector('.slider');
         const thumb = document.querySelector('.slider-thumb');
         const percent = data;
-        const color = `linear-gradient(90deg, var(--rojo) ${percent}%, var(--blanco) ${percent}%)`;
-        slider.style.background = color;
-        thumb.style.left = `calc(${percent}% - 12px)`;
+        if(textoBoton == "Menor"){
+            const color = `linear-gradient(90deg, var(--rojo) ${percent}%, var(--blanco) ${percent}%)`;
+            slider.style.background = color;
+            thumb.style.left = `calc(${percent}% - 12px)`;
+        }else{
+            const color = `linear-gradient(90deg, var(--blanco) ${percent}%, var(--rojo) ${percent}%)`;
+            slider.style.background = color;
+            thumb.style.right = `calc(${100-percent}% - 12px)`;
+        }
     }
 
     const generarNumero = () => {
@@ -54,6 +71,11 @@ export function Dice() {
             alert("SALIO: " + random + ", NO FLACO PERDISTE LA HERENCIA DE LA NONA")
         }
     }
+
+    const cambiarTexto = () => {
+        setMayorMenor(!mayorMenor);
+    }
+    const textoBoton = mayorMenor ? "Menor" : "Mayor";
     return (
         <>
             <section className="place-items-center border-[color:var(--violeta)] border-[20px] rounded-[30px] mx-[200px] mt-[150px] mb-[50px] h-[500px] gap-0 grid grid-cols-3 grid-rows-2 max-lg:mx-[20px] max-lg:grid-cols-1">
@@ -74,7 +96,10 @@ export function Dice() {
                         <input className="bg-[color:var(--negro)] bg-[color:var(--negro)] text-[color:var(--blanco)] rounded-[20px] p-2 w-[80%]" type="number" step="0.01" min="1.0102" max="9990" value={multi} onChange={calcularMulti}/>
                     </div>
                     <div>
-                        <label><h1>Menor</h1></label>
+                        <div className="flex">
+                            <label><h1>{textoBoton}</h1></label>
+                            <svg className="w-6 hover:cursor-pointer ml-2 hover:rotate-90 transition" onClick={cambiarTexto} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#fabc01" d="M142.9 142.9c62.2-62.2 162.7-62.5 225.3-1L327 183c-6.9 6.9-8.9 17.2-5.2 26.2s12.5 14.8 22.2 14.8H463.5c0 0 0 0 0 0H472c13.3 0 24-10.7 24-24V72c0-9.7-5.8-18.5-14.8-22.2s-19.3-1.7-26.2 5.2L413.4 96.6c-87.6-86.5-228.7-86.2-315.8 1C73.2 122 55.6 150.7 44.8 181.4c-5.9 16.7 2.9 34.9 19.5 40.8s34.9-2.9 40.8-19.5c7.7-21.8 20.2-42.3 37.8-59.8zM16 312v7.6 .7V440c0 9.7 5.8 18.5 14.8 22.2s19.3 1.7 26.2-5.2l41.6-41.6c87.6 86.5 228.7 86.2 315.8-1c24.4-24.4 42.1-53.1 52.9-83.7c5.9-16.7-2.9-34.9-19.5-40.8s-34.9 2.9-40.8 19.5c-7.7 21.8-20.2 42.3-37.8 59.8c-62.2 62.2-162.7 62.5-225.3 1L185 329c6.9-6.9 8.9-17.2 5.2-26.2s-12.5-14.8-22.2-14.8H48.4h-.7H40c-13.3 0-24 10.7-24 24z"/></svg>
+                        </div>
                         <input className="bg-[color:var(--negro)] text-[color:var(--blanco)] rounded-[20px] p-2 w-[80%]" type="number" min="1.0102" max="9990" value={data} />
                     </div>
                     <div>
--- App/app-react-casino/src/pages/Principal/Principal.tsx
+++ App/app-react-casino/src/pages/Principal/Principal.tsx
@@ -21,10 +21,10 @@ export function Principal() {
                 </div>
             </section>
             <section className='px-20 max-sm:px-3'>
-                <h1 className='pb-10 text-5xl text-[#70ae6e]'>Games</h1>
+                <h1 className='py-10 text-5xl text-[#70ae6e]'>Games</h1>
                 <div className='grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10 pb-20 rounded-xl max-sm:gap-2 text-center'>
-                    <Link to="/dice" className='max-md:col-span-2 md:col-span-2 lg:col-span-1 bg-red-500 p-20 border-2 text-white rounded-[40px]'>1</Link>
-                    <Link to="/tragamonedas" className='lg:col-span-2 p-20 bg-red-500 border-2 text-white rounded-[40px]'>2</Link>
+                    <Link to="/dice" className='max-md:col-span-2 md:col-span-2 lg:col-span-1 bg-red-500 p-20 border-2 text-white rounded-[40px] bg-[url("./src/assets/images/dice.webp")] bg-center lg:hover:scale-110 transition ease-in-out bg-cover hover:scale-[1.05]'></Link>
+                    <Link to="/tragamonedas" className='lg:col-span-2 p-20 bg-red-500 border-2 text-white rounded-[40px] bg-[url("./src/assets/images/slots.webp")] bg-center lg:hover:scale-110 transition ease-in-ou bg-cover hover:scale-[1.05]'></Link>
                     <Link to={""} className='lg:col-span-1 bg-red-500 p-20 border-2 text-white rounded-[40px]'>3</Link>
                     <Link to={""} className='max-md:col-span-2 md:col-span-2 lg:col-span-2 p-20 bg-red-500 border-2 text-white rounded-[40px]'>4</Link>
                     <Link to={""} className='md:col-span-2 lg:col-span-1 bg-red-500 p-20 border-2 text-white rounded-[40px]'>5</Link>
