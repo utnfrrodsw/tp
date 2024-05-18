d52d3969671073ac5128feaf21db7b3cf468a50f
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-05-18 12:36:27 -0300

    cambios

--- App/app-react-casino/src/components/LogIn/LogIn.tsx
+++ App/app-react-casino/src/components/LogIn/LogIn.tsx
@@ -15,6 +15,21 @@ export const LogIn = ({onClose}: {onClose: Function}) => {
                         <div className='login_form'>
                             <h2 className='loginTitle'>UTimbaN</h2>
                             <h3 className='loginSubTitle'>Login</h3>
+                            <form action="" method="" className='formulario'>
+                                <div className='name'>
+                                    <p className='formFrase'>Username or Email</p>
+                                    <input type="text" id='userId' className='ingresoForm' name='user_name'/>
+                                </div>
+                                <div className='password'>
+                                    <p className='formFrase'>Password</p>
+                                    <input type="password" id='password' className='ingresoForm' name='user_password'/>
+                                    <p>Forgot your password?</p>
+                                </div>
+                                <div className='submitClass'>
+                                    <button type='submit' className='boton_submit'>SUBMIT</button>
+                                    <p className='text-center'>REGISTRE FREE!</p>
+                                </div>
+                            </form>
                         </div>
                     </div>
                     <div className='login_rightside'>
