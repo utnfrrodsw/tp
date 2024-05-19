7352d3505b12e4516cdf9c52d9c0ce183698b47d
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-05-19 11:13:59 -0300

    cambios

--- App/app-react-casino/src/components/Games/Tragamonedas/Tragamonedas.tsx
+++ App/app-react-casino/src/components/Games/Tragamonedas/Tragamonedas.tsx
@@ -8,13 +8,32 @@ export function Tragamonedas() {
     )
     
     // Columnas correspondientes a la tragamonedas
-    const Reels = [0,1,2,3,4,5,6,7,8];
-    var max = Reels.length - 1
+    const Reels = ["Siete", "Banana", "Sandia", "Limon", "BAR", "Campana", "Naranja", "Fruta Violeta", "Fresa"];
+    const iconHeight = 79
+    var posicionOriginal = 1
 
     // Generador de un numero aleatorio mediante parametros, tiene un ERROR que necesita ser corregido y es que la generacion no incluye el minimo pero si el maximo
     function getRandomInt(min:number, max:number) {
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    const Spin = () => {
+        const Reel = document.querySelector(".reel") as HTMLElement
+        var numeroSorteo = getRandomInt(1,8)
+        var nuevaPosicion = posicionOriginal + numeroSorteo
+        posicionOriginal = nuevaPosicion
+        if (nuevaPosicion > 8) {
+            var resto = nuevaPosicion - 8
+            nuevaPosicion = 0 + --resto
+            posicionOriginal = nuevaPosicion
+        }
+        console.log(Reels[nuevaPosicion])
+        Reel.style.transition = `background-position-y 2000ms cubic-bezier(.41,-0.01,.63,1.09)`;
+        const style = getComputedStyle(Reel)
+        var bg = parseInt(style.backgroundPositionY)  
+        console.log(bg)
+        Reel.style.backgroundPositionY = `${(-iconHeight*(numeroSorteo)) + bg}px`;
+      }
 
     return(
 
@@ -24,8 +43,10 @@ export function Tragamonedas() {
 
             <div className="reel"></div>
 
-            <button className="buttonTragamonedas">Click para jugar</button>
+            <button className="buttonTragamonedas" onClick={Spin}>Click para jugar</button>
+
         </div>
+    
 
     )
 
