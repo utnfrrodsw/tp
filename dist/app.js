ef3c1524b79dc3d7a62017edb59827edb6485d03
JuliaMelgarejo <juliaamelgarejo@hotmail.com>
2024-05-20 19:46:19 -0300

    primera entrega

---
+++ dist/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+//import { animal } from './animalK.js';
+//animal--> /api/animal/
+//const animales = [
+//new animal(
+//'juan',
+//12/3/20,
+// 12/3/20,
+// )
+//];
+const app = express();
+app.use('./', (req, res) => {
+    res.send('hola');
+});
+app.listen(3000, () => {
+    console.log('server running on http://localhost:3000/');
+});
+//# sourceMappingURL=app.js.map
