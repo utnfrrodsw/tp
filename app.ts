e75389d61925ba2a3dd9669f7199eda542a5a21e
JuliaMelgarejo <juliaamelgarejo@hotmail.com>
2024-05-21 11:57:12 -0300

    crud

--- app.ts
+++ app.ts
@@ -1,6 +1,14 @@
 import express from 'express';
 import { animal } from './animal.js';
 
+
+const app = express();
+app.use(express.json());
+//midleware--> pequeños fragmentos de codigo en express que podemos incluir en 
+//nuestra cadena de codigo para la resolucion de una request 
+//estos van de a parte agregando, quitando y modificando la info de acuerdo a lo que sea necessario
+
+
 //animal--> /api/animal/
 const animales = [
   new animal(
@@ -10,11 +18,29 @@ const animales = [
   )
 ];
 
-const app = express();
-
 app.get('/api/animal',(req,res )=>{
   res.json(animales);
 })
+
+
+app.get('/api/animal/:id',(req,res )=>{
+  const animal = animales.find((animal) => animal.id === req.params.id);
+  if(!animal){
+    res.status(404).send({message:'ID incorrecto, no existe ningun animal con ese ID' })
+  }
+  res.json;{animal}
+})
+
+app.post('/api/animal',(req,res )=>{
+  const {nombre, fechaRescate, fechaNacimientoEStimativa} = req.body
+
+  const animales2 = new animal (nombre, fechaRescate, fechaNacimientoEStimativa); 
+
+  animales.push(animales2)
+  res.status(201).send({message: 'animal agregado correctamente', data: animal })
+})
+
+
 
  app.listen(3000, ()=>{
   console.log('server running on http://localhost:3000/');
