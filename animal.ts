58adf8895b475b69805bc6dfe607e82bce27e59e
unknown <tomasperesin@gmail.com>
2024-06-05 22:33:55 -0300

    ABM animal

--- animal.ts
+++ animal.ts
@@ -3,8 +3,8 @@ import crypto from 'node:crypto';
 export class animal{
   constructor(
     public nombre: string, 
-    public fechaRescate: number,
-    public fechaNacimientoEStimativa: number,
-    public id = crypto.randomUUID()
+    public fechaRescate: string,
+    public fechaNacimientoEStimativa: string,
+    public id: string
   ){}
 }
