d20685be2e35a770dbfdefe4c1447162ed57ec98
Nicolas Herrera <herreranico2703@gmail.com>
2024-06-12 16:47:35 -0300

    CRUD Producto

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import { animal } from './animal.js';
+import { producto } from './producto.js';
 
 
 const app = express();
@@ -94,10 +95,95 @@ app.delete('/api/animal/:id',(req,res )=>{
   res.status(200).send({message: 'animal eliminado correctamente'})
 })
 
+//PRODUCTO --> /api/producto/
+
+const productos = [
+  new producto(
+    'Alimento',
+    'Alimento balanceado para perro',
+    100,
+    '01'
+  ),
+];
+
+function sanitizeProductoInput(req: Request, res: Response, next:NextFunction){
+  
+  req.body.sanitizedProducto = {
+    nombre: req.body.nombre,
+    descripcion: req.body.descripcion,
+    stock: req.body.stock
+  }
+
+  Object.keys(req.body.sanitizedProducto).forEach((key) => {
+    if (req.body.sanitizedProducto[key] === undefined) {
+      delete req.body.sanitizedProducto[key]
+    }
+  })
+
+  next()
+}
+
+app.get('/api/producto',(req,res )=>{
+  res.json(productos);
+})
+
+
+app.get('/api/producto/:id',(req,res )=>{
+  const producto = productos.find((producto) => producto.id === req.params.id);
+  if(!producto){
+    return res.status(404).send({message:'ID incorrecto, no existe ningun producto con ese ID' })
+  }
+  res.json(producto)
+})
+
+
+app.post('/api/producto', sanitizeProductoInput, (req,res) => {
+  const {nombre, descripcion, stock, id} = req.body
+
+  const productos2 = new producto (nombre, descripcion, stock, id ); 
+
+  productos.push(productos2)
+  return res.status(201).send({message: 'Producto agregado correctamente', data: producto })
+})
+
+
+app.put ('/api/producto/:id', sanitizeProductoInput, (req,res) => {
+  const productoIdx = productos.findIndex((producto) => producto.id === req.params.id);
+  if (productoIdx === -1) {
+    res.status(404).send({message:'ID incorrecto, no existe ningun producto con ese ID' })
+  }
+
+  productos[productoIdx]= {...productos[productoIdx], ...req.body.sanitizedProducto };
+
+  res.status(200).send({message: 'Producto modificado correctamente', data: productos[productoIdx] })
+})
+
+
+app.patch ('/api/producto/:id', sanitizeProductoInput, (req,res )=>{
+  const productoIdx = productos.findIndex((producto) => producto.id === req.params.id);
+  if (productoIdx === -1) {
+    return res.status(404).send({message:'ID incorrecto, no existe ningun producto con ese ID' })
+  }
+
+  productos[productoIdx]= {...productos[productoIdx], ...req.body.sanitizedProducto };
+
+  res.status(200).send({message: 'Producto modificado correctamente', data: productos[productoIdx]  })
+})
+
+
+app.delete('/api/producto/:id',(req,res )=> {
+  const productoIdx = productos.findIndex((producto) => producto.id === req.params.id);
+  if (productoIdx === -1) {
+    res.status(404).send({message:'ID incorrecto, no existe ningun producto con ese ID' })
+  }
+  productos.splice(productoIdx, 1);
+  res.status(200).send({message: 'Producto eliminado correctamente'})
+})
+
 app.listen(3000, ()=>{
 console.log('server running on http://localhost:3000/');
 })
 
- //put--> se utiliza para modificar el objeto entero
-   // patch--> se utiliza para modificar parcialmente el objeto, osea algunos atributos "/*".
+//put--> se utiliza para modificar el objeto entero
+// patch--> se utiliza para modificar parcialmente el objeto, osea algunos atributos "/*".
    
---
+++ dist/producto.js
@@ -0,0 +1,9 @@
+export class producto {
+    constructor(nombre, descripcion, stock, id) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.stock = stock;
+        this.id = id;
+    }
+}
+//# sourceMappingURL=producto.js.map
