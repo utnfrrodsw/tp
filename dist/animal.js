f93b2c8264c5f9305986002903eec8d39158db15
Nicolas Herrera <herreranico2703@gmail.com>
2024-06-27 16:57:33 -0300

    Creacion monorepo
    Creacion de carpetas de back y front
    Boilerplate del proyecto de angular

--- dist/animal.js
+++
@@ -1,9 +0,0 @@
-export class animal {
-    constructor(nombre, fechaRescate, fechaNacimientoEStimativa, id) {
-        this.nombre = nombre;
-        this.fechaRescate = fechaRescate;
-        this.fechaNacimientoEStimativa = fechaNacimientoEStimativa;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=animal.js.map
--- dist/veterinaria.js
+++
@@ -1,8 +0,0 @@
-export class veterinaria {
-    constructor(nombre, direccion, id) {
-        this.nombre = nombre;
-        this.direccion = direccion;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=veterinaria.js.map
