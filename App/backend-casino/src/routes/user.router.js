cbe9da9e1e71ae79935152c6fc0689be9ae52e1f
Agus Gil <agussv360@gmail.com>
2024-07-11 16:44:42 -0300

    Update user.router.js

--- App/backend-casino/src/routes/user.router.js
+++ App/backend-casino/src/routes/user.router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router
-    .get("/", userController.get )
+    .get("/", userController.get)
     .get("/:id", userController.getById)
     .post("/", userController.create)
     .put("/:id", userController.update)
