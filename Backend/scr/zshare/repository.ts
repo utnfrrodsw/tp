722b657da943d098fa420da15d18d286bfedc0d0
unknown <tomasperesin@gmail.com>
2024-07-13 12:24:45 -0300

    MVC animal

--- Backend/scr/zshare/repository.ts
+++ Backend/scr/zshare/repository.ts
@@ -1,7 +1,7 @@
-export interface repository<T> {
-  findall(): T[] | undefined
+export interface Repository<T> {
+  findAll(): T[] | undefined
   findOne(item: {id: string}): T | undefined
   add(item: T): T | undefined
   update(item: T): T | undefined
-  delete(item: T): T | undefined
+  delete(item: { id: string }): T | undefined
 }
