8caa1cb20a27d991826d8a2ee63e82ed8e053bef
Lucas <lucasmaggi03@gmail.com>
2024-08-03 16:20:30 -0300

    add category, game bd

--- App/backend-casino/src/db/models/index.js
+++ App/backend-casino/src/db/models/index.js
@@ -1,12 +1,17 @@
 const { User, UserSchema } = require('./user.model');
 const { Country, CountrySchema } = require('./countries.model');
 const { State, StateSchema } = require('./state.model');
+const { Location, LocationSchema } = require('./location.model');
+const { Category, CategorySchema } = require('./category.model');
+const { Game, GameSchema } = require('./game.model');
 
 function setupModels(sequelize){
     User.init(UserSchema, User.config(sequelize));
     Country.init(CountrySchema, Country.config(sequelize));
     State.init(StateSchema, State.config(sequelize));
-    
+    Location.init(LocationSchema, Location.config(sequelize));
+    Category.init(CategorySchema, Category.config(sequelize));
+    Game.init(GameSchema, Game.config(sequelize));
 
 }
 
--- App/backend-casino/src/routes/index.js
+++ App/backend-casino/src/routes/index.js
@@ -3,6 +3,9 @@ const express = require('express');
 const userRouter = require ('./user.router');
 const countryRouter = require ('./country.router');
 const stateRouter = require ('./state.router');
+const locationRouter = require ('./location.router');
+const categoryRouter = require ('./category.router');
+const gameRouter = require ('./game.router');
 
 function routerApi(app) {
     const router = express.Router();
@@ -10,6 +13,9 @@ function routerApi(app) {
     router.use('/countries', countryRouter);
     router.use('/users', userRouter);
     router.use('/states', stateRouter);
+    router.use('/locations', locationRouter);
+    router.use('/categories', categoryRouter);
+    router.use('/games', gameRouter);
 }
 
 module.exports = routerApi;
