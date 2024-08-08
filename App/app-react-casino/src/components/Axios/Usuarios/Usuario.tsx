433a5ac0b5add229abd926ef52d96529e504496c
Lucas <lucasmaggi03@gmail.com>
2024-08-07 21:24:55 -0300

    user list

--- App/app-react-casino/src/components/Axios/Usuarios/Usuario.tsx
+++ App/app-react-casino/src/components/Axios/Usuarios/Usuario.tsx
@@ -17,18 +17,35 @@ export function Usuario() {
     });
 
     return (
-        <>
-        <div className="hola">
-                <ul>
-                    {user.map((item) => {
-                        return (
-                        <li key={item.id_user}> {item.id_user} {item.username} {item.role} {item.email} {item.balance} </li>
-                        );
-                    })}
-                </ul>
+        <div className="list-main">
+            <div className='titulo-'>
+                <h2 className='titulo-user'>Gestión de Usuarios</h2>
+            </div>
+            <div className="header-name">
+                <div className="header-column">ID</div>
+                <div className="header-column">Username</div>
+                <div className="header-column">Role</div>
+                <div className="header-column">Email</div>
+                <div className="header-column">Balance</div>
+                <div className="header-column">Actions</div>
+            </div>
+            <ul className='list'>
+                {user.map((item) => (
+                    <li className='items' key={item.id_user}>
+                        <div className="user-info">
+                            <div className="column">{item.id_user}</div>
+                            <div className="column">{item.username}</div>
+                            <div className="column">{item.role}</div>
+                            <div className="column">{item.email}</div>
+                            <div className="column">{item.balance}</div>
+                        </div>
+                        <div className="actions">
+                            <button className="edit-btn">Edit</button>
+                            <button className="delete-btn">Delete</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
         </div>
-        </>
-
-    )
-
+    );
 }
