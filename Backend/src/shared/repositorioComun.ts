51ca0df82096f2884a79d133881b281b261f2d56
Tomi Yasparra <66183057+YasperMaster@users.noreply.github.com>
2024-08-08 16:55:28 -0300

    Delete Backend/src directory

--- Backend/src/shared/repositorioComun.ts
+++
@@ -1,7 +0,0 @@
-export interface Repository<T> {
-    findAll(): T[] | undefined
-    findOne(item: { id: number }): T | undefined
-    add(item: T): T | undefined
-    update(item: T): T | undefined
-    delete(item: {id: number}): T | undefined 
-}
