f7778188b84cf29122604195e07af50b3b96d7f0
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-08-09 12:53:04 -0300

    ajuste y arreglo de sidebar

--- App/app-react-casino/src/components/GamesSideBar.tsx
+++ App/app-react-casino/src/components/GamesSideBar.tsx
@@ -10,6 +10,7 @@ export function GamesSideBar() {
             <Link to="/dice"><img src="./src/assets/images/dice.webp" alt="DICE" className="game"/></Link>
             <Link to="/slot"><img src="./src/assets/images/slots.webp" alt="Slots" className="game"/></Link>
             <Link to="/wheel"><img src="./src/assets/images/wheel.webp" alt="Mega Wheel" className="game"/></Link>
+            <div className="SideBarMobileButton"></div>
         </div>
     )
 
