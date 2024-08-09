bc556c7ed4fb63fc4d4547e2da5ebef91b24b35f
Tomi Yasparra <66183057+YasperMaster@users.noreply.github.com>
2024-08-08 22:04:34 -0300

    Delete Backend directory

--- Backend/src/shared/repository.ts
+++
@@ -1,7 +0,0 @@
-export interface Repository<T> {
-    findAll(): T[] | undefined
-    findOne(item: { id: string }): T | undefined
-    add(item: T): T | undefined
-    update(item: T): T | undefined
-    delete(item: { id: string }): T | undefined
-  }
