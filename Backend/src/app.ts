2319434d9dc20faf03818caadc86b1f154881c54
cufardixx <facundopicia@gmail.com>
2024-08-10 16:09:23 -0300

    solucion de cors

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import { userRouter } from "./user/user.routes.js";
 import { eventoRouter } from "./event/event.routes.js";
+import cors from 'cors'
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 app.use("/api/users", userRouter)
 app.use("/api/evento", eventoRouter)
