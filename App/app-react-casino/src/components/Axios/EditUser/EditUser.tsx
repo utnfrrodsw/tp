34da23c042253e405fc670c8493cd00f6e5bf144
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-08-11 10:47:48 -0300

    edituser completado

--- App/app-react-casino/src/components/Axios/EditUser/EditUser.tsx
+++ App/app-react-casino/src/components/Axios/EditUser/EditUser.tsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import './EditUser.css';
 import { useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import { useEffect } from 'react';
+import { useRef } from 'react';
 import { NavLink as Link } from 'react-router-dom'
 
 export function EditUser() {
@@ -10,15 +9,15 @@ export function EditUser() {
     const usuario = (useLocation().state)
     console.log(usuario.id_user)
 
-    function patchUser() {
+    function patchUser(datos) {
+        console.log(datos)
         axios.put(`http://localhost:3000/api/v1/users/${usuario.id_user}`, {
-            first_name: 'mauridd',
-            last_name: 'fiorindd',
-            street: 'la maldita ca',
-            phone: '3424125',
-            email: 'dasdasd@gmail.com',
-            password: 'a345',
-            balance: '21',
+            first_name: `${datos.firstname.value}`,
+            last_name: `${datos.lastname.value}`,
+            street: `${datos.street.value}`,
+            phone: `${datos.phone.value}`,
+            email: `${datos.email.value}`,
+            password: `${datos.password.value}`,
         })
         .then((response) => {
             console.log(response);
@@ -28,11 +27,13 @@ export function EditUser() {
         });
     }
 
+    const form = useRef()
+
     return (
         
         <>
             <div className='edituser'>
-                <form action="" className='formEdit'>
+                <form action="" className='formEdit' ref={form}>
                     <input type="text" id='firstname' className='formInput' placeholder={usuario.first_name} name='firstname'/>
                     <input type="text" id='lastname' className='formInput' placeholder={usuario.last_name} name='lastname'/>
                     <input type="text" id='street' className='formInput' placeholder={usuario.street} name='street'/>
@@ -40,7 +41,7 @@ export function EditUser() {
                     <input type="text" id='email' className='formInput' placeholder={usuario.email} name='email'/>
                     <input type="text" id='password' className='formInput' placeholder={usuario.password} name='password'/>
                 </form>
-                <button type='submit' className='formSubmit' onClick={() => patchUser()}>Actualizar</button>
+                <button type='submit' className='formSubmit' onClick={() => patchUser(form.current)}>Actualizar</button>
                 <Link to="/user" className="back">volver</Link>
             </div>
         </>
