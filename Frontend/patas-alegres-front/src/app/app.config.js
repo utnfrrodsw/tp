93e9d6357fb39c4516a3297a87143565235f04be
JuliaMelgarejo <juliaamelgarejo@hotmail.com>
2024-08-13 10:51:54 -0300

    Revert "x"
    This reverts commit 356cdfe9b1074d3513f54fb703c58f1c2ce6004d.

--- Frontend/patas-alegres-front/src/app/app.config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.appConfig = void 0;
-const core_1 = require("@angular/core");
-const router_1 = require("@angular/router");
-const app_routes_1 = require("./app.routes");
-exports.appConfig = {
-    providers: [(0, core_1.provideZoneChangeDetection)({ eventCoalescing: true }), (0, router_1.provideRouter)(app_routes_1.routes)]
-};
