409d6999b9cb59fec4ae561ed0279226a211c42f
Lucas <lucasmaggi03@gmail.com>
2024-09-01 13:27:05 -0300

    1st commit mp, client and server

---
+++ App/app-react-casino/src/components/BalanceModal/BalanceModal.tsx
@@ -0,0 +1,57 @@
+import './BalanceModal.css';
+import { initMercadoPago, Wallet } from '@mercadopago/sdk-react';
+import axios from "axios";
+import { useState } from 'react';
+
+interface BalanceModalProps {
+    onClose: () => void;
+}
+
+export const BalanceModal: React.FC<BalanceModalProps> = ({ onClose }) => {
+    const [preferenceId, setPreferenceId] = useState(null);
+
+    initMercadoPago('APP_USR-03fdd897-d911-496d-8c09-61802b128fa3', {
+        locale: "es-AR",
+    });
+
+    const createPreference = async () => {
+        try{
+            const response = await axios.post("http://localhost:3000/create_preference", {
+                title: "Tip",
+                quantity: 1,
+                price: 100,
+            });
+
+        const{id} = response.data;
+        return id;
+    }   catch(error){
+        console.log(error);
+    }
+};
+    const handleBuy = async () =>{
+        const id = await createPreference();
+        if (id){
+            setPreferenceId(id);
+        }
+    }
+
+    return (
+        <div className="modal-overlay" onClick={onClose}>
+            <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+                <button className="close-button" onClick={onClose}>X</button>
+                <div className="modal-body">
+                    <h2>Selecciona una opcion de deposito!</h2>
+                    <div className="tips">
+                        <div onClick={handleBuy} className="tip">$1000</div>
+                        <div onClick={handleBuy}  className="tip">$2000</div>
+                        <div onClick={handleBuy}  className="tip">$5000</div>
+                        <div onClick={handleBuy}  className="tip">$10.000</div>
+                        <button onClick={handleBuy}  className="tip">$25.000</button>
+                        {preferenceId && <Wallet initialization={{ preferenceId: preferenceId }} />}                       
+                    </div>
+                    <p>Mercada Pago unica opción</p>
+                </div>
+            </div>
+        </div>
+    );
+};
