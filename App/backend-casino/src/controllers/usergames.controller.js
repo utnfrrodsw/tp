a43586f119f5c6fe0453a056cb943e7e180fb64d
Agus Gil <agussv360@gmail.com>
2024-09-11 16:49:59 -0300

    Back-End Update
    Create UserGames = model, controller, route, service
    
    Only services added Create and findAll -> will change in the future with querys.

---
+++ App/backend-casino/src/controllers/usergames.controller.js
@@ -0,0 +1,24 @@
+const UsersGamesService = require('../services/usergames.service');
+const service = new UsersGamesService();
+
+const create = async(req, res) => {
+    try{
+        const response = await service.create(req.body)
+        res.json({ success: true, data: response })
+    } catch(error) {
+        res.status(500).send({ success: false, message: error.message })
+    }
+}
+
+const get = async(req, res) => {
+    try{
+        const response = await service.find();
+        res.json(response);
+    } catch(error) {
+        res.status(500).send({ success: false, message: error.message })
+    }
+}
+
+module.exports = {
+    create, get
+};
