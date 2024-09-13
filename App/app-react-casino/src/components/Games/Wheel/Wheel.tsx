ae2ec19fe34c1166b6239b39c0e4e2e3b0002d67
Lucas <lucasmaggi03@gmail.com>
2024-09-12 22:50:25 -0300

    wheel sound

--- App/app-react-casino/src/components/Games/Wheel/Wheel.tsx
+++ App/app-react-casino/src/components/Games/Wheel/Wheel.tsx
@@ -1,9 +1,13 @@
-import { useState, ChangeEvent, useEffect} from 'react';
+import { useState, ChangeEvent, useEffect } from 'react';
 import './Wheel.css';
 import { GamesSideBar } from '../../GamesSideBar';
 import WheelImage from '../../../assets/images/wheel.png';
 import WheelPointer from '../../../assets/images/wheelPointer.png';
 import axios from '../../Axios/axios';
+import wheelSpinSound from "../../../assets/sounds/wheelSpin.mp3";
+import wheelWinSound from "../../../assets/sounds/wheelWin.mp3";
+import wheelLoseSound from "../../../assets/sounds/wheelLose.mp3";
+import mutedIcon from "../../../assets/images/mutedIcon.png";
 
 const colors = [
   'verde', 'gris', 'verde', 'gris', 'amarillo', 'gris', 'verde', 'gris',
@@ -16,6 +20,27 @@ let rotation: number = 0;
 let index: number = 0;
 
 export function Wheel(user) {
+  const [selectedColor, setSelectedColor] = useState<string>("");
+  const [monto, setMonto] = useState(0);
+  const [ganarVerde, setRecibeGanar] = useState(0);
+  const [ganarBlanco, setRecibeGanarB] = useState(0);
+  const [ganarAmarillo, setRecibeGanarA] = useState(0);
+  const [ganarVioleta, setRecibeGanarV] = useState(0);
+  const [ganarNaranja, setRecibeGanarN] = useState(0);
+  const [isSpinning, setIsSpinning] = useState(false);
+  const [error, setError] = useState("");
+  const [isActive, setIsActive] = useState(true)
+  const [isMuted, setIsMuted] = useState(false)
+
+  const handleToggle = () => {
+    setIsMuted(!isMuted)
+}
+
+  const anglePerSection = 360 / 30;
+
+  const wheelSpin = new Audio(wheelSpinSound);
+  const wheelWin = new Audio(wheelWinSound);
+  const wheelLose = new Audio(wheelLoseSound);
 
   function patchUser(newMoney) {
     axios.put(`http://localhost:3000/api/v1/users/${user.id}`, {
@@ -47,55 +72,80 @@ export function Wheel(user) {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
-  const [selectedColor, setSelectedColor] = useState<string>("");
-  const [monto, setMonto] = useState(0);
-  const [ganarVerde, setRecibeGanar] = useState(0);
-  const [ganarBlanco, setRecibeGanarB] = useState(0);
-  const [ganarAmarillo, setRecibeGanarA] = useState(0);
-  const [ganarVioleta, setRecibeGanarV] = useState(0);
-  const [ganarNaranja, setRecibeGanarN] = useState(0);
 
-  const anglePerSection = 360 / 30;
+  const playSound = (sound) => {
+    sound.currentTime = 0;
+    if (isMuted == false) {
+      sound.play();
+  }
+  }
 
   const winning = (colour) => {
-    if (colour == "verde") {
-      user.onMoney(user.balance + ganarVerde - monto)
-      patchUser(user.balance + ganarVerde - monto)
-      postGame(monto, ganarVerde)
-    } else if (colour == "blanco") {
-      user.onMoney(user.balance + ganarBlanco - monto)
-      patchUser(user.balance + ganarBlanco - monto)
-      postGame(monto, ganarBlanco)
-    } else if (colour == "amarillo") {
-      user.onMoney(user.balance + ganarAmarillo - monto)
-      patchUser(user.balance + ganarAmarillo - monto)
-      postGame(monto, ganarAmarillo)
-    } else if (colour == "violeta") {
-      user.onMoney(user.balance + ganarVioleta - monto)
-      patchUser(user.balance + ganarVioleta - monto)
-      postGame(monto, ganarVioleta)
-    } else if (colour == "naranja") {
-      user.onMoney(user.balance + ganarNaranja- monto)
-      patchUser(user.balance + ganarNaranja - monto)
-      postGame(monto, ganarNaranja)
+    if (colour === "verde") {
+      user.onMoney(user.balance + ganarVerde - monto);
+      patchUser(user.balance + ganarVerde - monto);
+      postGame(monto, ganarVerde);
+      playSound(wheelWin);
+    } else if (colour === "blanco") {
+      user.onMoney(user.balance + ganarBlanco - monto);
+      patchUser(user.balance + ganarBlanco - monto);
+      postGame(monto, ganarBlanco);
+      playSound(wheelWin);
+    } else if (colour === "amarillo") {
+      user.onMoney(user.balance + ganarAmarillo - monto);
+      patchUser(user.balance + ganarAmarillo - monto);
+      postGame(monto, ganarAmarillo);
+      playSound(wheelWin);
+    } else if (colour === "violeta") {
+      user.onMoney(user.balance + ganarVioleta - monto);
+      patchUser(user.balance + ganarVioleta - monto);
+      postGame(monto, ganarVioleta);
+      playSound(wheelWin);
+    } else if (colour === "naranja") {
+      user.onMoney(user.balance + ganarNaranja - monto);
+      patchUser(user.balance + ganarNaranja - monto);
+      postGame(monto, ganarNaranja);
+      playSound(wheelWin);
+    } else if (colour === "gris") {
+      postGame(monto, 0);
+      playSound(wheelLose);
     } else {
-      postGame(monto, 0)
+      postGame(monto, 0);
     }
   }
 
   const startSpin = () => {
-    user.onMoney(user.balance - monto)
-    patchUser(user.balance - monto)
-    const wheelStyle = document.querySelector(".wheel") as HTMLElement
+    if (user.balance <= 0) {
+      setError("Insufficient Balance");
+      return;
+    }
+    if (monto <= 0 || monto > user.balance) {
+      setError("Invalid amount");
+      return;
+    }else{
+      setError("")
+    }
+  
+    if (isSpinning) return;
+
+    setIsSpinning(true);
+    user.onMoney(user.balance - monto);
+    patchUser(user.balance - monto);
+    
+    const wheelStyle = document.querySelector(".wheel") as HTMLElement;
     const randomIndex = Math.floor(Math.random() * 30);
     const selectedColor = colors[randomIndex];
     setSelectedColor(selectedColor);
     rotation += 3 * 360 + anglePerSection * (index - randomIndex);
     wheelStyle.style.transform = `rotate(${rotation}deg)`;
     index = randomIndex;
-    setTimeout(function() {
+    
+    playSound(wheelSpin);
+
+    setTimeout(() => {
       winning(selectedColor);
-    }, 4000);
+      setIsSpinning(false);
+    }, 6000);
   }
 
   const montoApuesta = (event: ChangeEvent<HTMLInputElement>) => {
@@ -106,49 +156,55 @@ export function Wheel(user) {
     calcularAmarillo(monto);
     calcularVioleta(monto);
     calcularNaranja(monto);
-}
+  }
 
-const calcularVerde = (monto:number) => {
-  const ganarVerde = parseFloat((1.5 * monto).toFixed(2));
-  setRecibeGanar(ganarVerde)
-}
-const calcularBlanco = (monto:number) => {
-  const ganarBlancoB = parseFloat((1.7 * monto).toFixed(2));
-  setRecibeGanarB(ganarBlancoB)
-}
-const calcularAmarillo = (monto:number) => {
-  const ganarAmarillo = parseFloat((2 * monto).toFixed(2));
-  setRecibeGanarA(ganarAmarillo)
-}
-const calcularVioleta = (monto:number) => {
-  const ganarVioleta = parseFloat((3 * monto).toFixed(2));
-  setRecibeGanarV(ganarVioleta)
-}
-const calcularNaranja = (monto:number) => {
-  const ganarNaranja = parseFloat((4 * monto).toFixed(2));
-  setRecibeGanarN(ganarNaranja)
-}
+  const calcularVerde = (monto: number) => {
+    const ganarVerde = parseFloat((1.5 * monto).toFixed(2));
+    setRecibeGanar(ganarVerde);
+  }
+  
+  const calcularBlanco = (monto: number) => {
+    const ganarBlancoB = parseFloat((1.7 * monto).toFixed(2));
+    setRecibeGanarB(ganarBlancoB);
+  }
+  
+  const calcularAmarillo = (monto: number) => {
+    const ganarAmarillo = parseFloat((2 * monto).toFixed(2));
+    setRecibeGanarA(ganarAmarillo);
+  }
+  
+  const calcularVioleta = (monto: number) => {
+    const ganarVioleta = parseFloat((3 * monto).toFixed(2));
+    setRecibeGanarV(ganarVioleta);
+  }
+  
+  const calcularNaranja = (monto: number) => {
+    const ganarNaranja = parseFloat((4 * monto).toFixed(2));
+    setRecibeGanarN(ganarNaranja);
+  }
 
   return (
     <div className="place-items-center border-[color:var(--violeta)] border-[20px] rounded-[30px] mx-[100px] mt-[150px] mb-[50px] h-[700px] gap-0 grid grid-cols-3 grid-rows-2 max-lg:mx-[20px] max-lg:grid-cols-1">
       <GamesSideBar/>
       <div className="col-span-1 row-span-2 bg-[color:var(--violeta)] w-full h-full p-2 flex flex-col justify-center items-center">
-          <label className="">Monto de Apuesta</label>
-          <input step="0.01" className="bg-[color:var(--blanco)] text-black rounded-[20px] p-2 w-[80%] mb-2" min="0" value={monto} onChange={montoApuesta}/>
-          <label className="">Gris <span className='font-bold'>x0.00</span></label>
-          <input className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
-          <label className="">Verde <span className='font-bold'>x1.50</span></label>
-          <input className="text-black rounded-[20px] p-2 w-[80%]" value={ganarVerde} type="number" inputMode="decimal" placeholder="0.00" disabled/>
-          <label className="">Blanco <span className='font-bold'>x1.70</span></label>
-          <input value={ganarBlanco} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
-          <label className="">Amarillo <span className='font-bold'>x2.00</span></label>
-          <input value={ganarAmarillo} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
-          <label className="">Violeta <span className='font-bold'>x3.00</span></label>
-          <input value={ganarVioleta} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
-          <label className="">Naranja <span className='font-bold'>x4.00</span></label>
-          <input value={ganarNaranja} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
-          <button className="button bg-[color:var(--amarillo)] hover:bg-yellow-600 text-[color:var(--negro)] py-3 w-[80%] mt-[20px] text-bold max-lg:mb-[20px] " onClick={startSpin}>APOSTAR</button>
-          <p className='py-5'>El color seleccionado es: {selectedColor}</p>
+      <button className={isMuted ? "mutedButton mutedEnabled" : "mutedButton"} onClick={handleToggle}><img src={mutedIcon} alt="mutedIcon"/></button>
+        <label className="">Stake amount</label>
+        <input step="0.01" className="bg-[color:var(--blanco)] text-black rounded-[20px] p-2 w-[80%] mb-2" min="0" value={monto} onChange={montoApuesta}/>
+        <label className="">Gray <span className='font-bold'>x0.00</span></label>
+        <input className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
+        <label className="">Green <span className='font-bold'>x1.50</span></label>
+        <input className="text-black rounded-[20px] p-2 w-[80%]" value={ganarVerde} type="number" inputMode="decimal" placeholder="0.00" disabled/>
+        <label className="">White <span className='font-bold'>x1.70</span></label>
+        <input value={ganarBlanco} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
+        <label className="">Yellow <span className='font-bold'>x2.00</span></label>
+        <input value={ganarAmarillo} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
+        <label className="">Purple <span className='font-bold'>x3.00</span></label>
+        <input value={ganarVioleta} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
+        <label className="">Orange <span className='font-bold'>x4.00</span></label>
+        <input value={ganarNaranja} className="text-black rounded-[20px] p-2 w-[80%]" type="number" inputMode="decimal" placeholder="0.00" disabled/>
+        <button 
+          className="button bg-[color:var(--amarillo)] hover:bg-yellow-600 text-[color:var(--negro)] py-3 w-[80%] mt-[20px] text-bold max-lg:mb-[20px]" onClick={startSpin} disabled={isSpinning}>BET</button>
+        <p className='py-5'>{error}</p>
       </div>
       <div className='relative col-span-2 flex justify-center items-start pt-[300px]'>
         <img className='absolute z-20 top-400 left-1/2 -translate-x-1/2' src={WheelPointer} alt="" />
