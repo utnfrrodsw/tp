dd69f9c68edd66aaff8a03070190ba34f4986d06
Mauricio Fiorini <mauriciofiorini911@gmail.com>
2024-10-08 18:50:48 -0300

    Update Details.tsx

--- App/app-react-casino/src/components/Axios/Details/Details.tsx
+++ App/app-react-casino/src/components/Axios/Details/Details.tsx
@@ -10,9 +10,6 @@ export function Details() {
     console.log(useLocation().state)
     const usuario = (useLocation().state)
     const [data, setData] = useState([])
-    const [country, setCountry] = useState([]);
-    const [city, setCity] = useState([]);
-    const [province, setProvince] = useState([]);
 
     const GetData = () => {
         axios.get(`http://localhost:3000/api/v1/users/read/${usuario.id_user}`).then((response) =>
@@ -20,50 +17,15 @@ export function Details() {
         );
     };
 
-    const GetCountry = () => {
-        axios.get(`http://localhost:3000/api/v1/countries/${usuario.id_country}`).then((responseCountry) =>
-            setCountry(responseCountry.data)
-        );
-    };
-
-    const GetCity = () => {
-        axios.get(`http://localhost:3000/api/v1/cities/${usuario.id_city}`).then((responseCity) =>
-            setCity(responseCity.data)
-        );
-    };
-
-    const GetProvince = () => {
-        axios.get(`http://localhost:3000/api/v1/provinces/${usuario.id_province}`).then((responseProvince) =>
-            setProvince(responseProvince.data)
-        );
-    };
-
-
     useEffect(() => {
-        GetCountry();
-        GetCity();
-        GetProvince();
+        GetData();
     }, []);
-
-
 
     return (
         
         <>
             <div className='details'>
                 <p>La id del usuario es {usuario.id_user}</p>
-                <p>nombre: {usuario.first_name}</p>
-                <p>apellido: {usuario.last_name}</p>
-                <p>fecha de nacimiento: {usuario.birthday}</p>
-                <p>direccion: {usuario.street}</p>
-                <p>telefono: {usuario.phone}</p>
-                <p>correo: {usuario.email}</p>
-                <p>balance: {usuario.balance}</p>
-                <p>pais: {country.nice_name}</p>
-                <p>numero pais: {country.num_code}</p>
-                <p>ciudad: {city.name}</p>
-                <p>codigo postal: {city.postal_code}</p>
-                <p>provincia: {province.name}</p>
             </div>
 
             <Link to="/user" className="back">volver</Link>
