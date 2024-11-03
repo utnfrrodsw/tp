3065f5b23820101b16eaba9edd3aa2e798f92143
Lucas <lucasmaggi03@gmail.com>
2024-11-02 21:38:36 -0300

    bug fixes and query update

--- App/backend-casino/src/services/usergames.service.js
+++ App/backend-casino/src/services/usergames.service.js
@@ -24,11 +24,12 @@ class UsersGamesService{
 
     async leaderboard(){
         const res = await models.UserGame.sequelize.query(`select ug.id_user, us.username, ug.bet, max(ug.winning) as winning, ga.name as game
-        from UserGames ug
-        inner join Users us on ug.id_user = us.id_user
-        inner join Games ga on ug.id_game = ga.id_game
-        where ug.winning > 0
-        group by ug.id_user, us.username, ga.name, ug.bet`, {
+        from usergames ug
+        inner join users us on ug.id_user = us.id_user
+        inner join games ga on ug.id_game = ga.id_game
+        where ug.winning > 0 and ga.id_game in (1, 3)
+        group by ug.id_user, us.username, ga.name, ug.bet
+        limit 10;`, {
             type: QueryTypes.SELECT
         })
         return res;
