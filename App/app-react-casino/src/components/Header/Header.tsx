8bfadff2e8fa4f82bd80c85ce9037c816331bad6
Agus Gil <agussv360@gmail.com>
2024-11-07 17:41:34 -0300

    Header Responsive
    - Icons
    - CSS
    - All Roles

--- App/app-react-casino/src/components/Header/Header.tsx
+++ App/app-react-casino/src/components/Header/Header.tsx
@@ -3,13 +3,19 @@ import { NavLink as Link, useNavigate } from 'react-router-dom';
 import { LogIn } from '../index.js'
 import { BalanceModal } from './BalanceModal/BalanceModal.js';
 
+
 import { IoPersonSharp } from "react-icons/io5";
 import { FiLogOut } from "react-icons/fi";
 import { MdLeaderboard } from "react-icons/md";
+import { LuMenu } from "react-icons/lu";
+import { IoIosClose } from "react-icons/io";
+import { GrCircleInformation } from "react-icons/gr";
+import { MdOutlineAdminPanelSettings } from "react-icons/md";
 
 import { useState } from 'react';
 import HeaderToggle from './HeaderToggle/HeaderToggle.js';
 import HeaderMenu from './HeaderMenu/HeaderMenu.js';
+
 
 import './Header.css';
 
@@ -26,7 +32,7 @@ export function Header({ balance, profile, role, username, setMoney, idUser }: H
     const [modalOpen, setModalOpen] = useState(false);
     const [open, setMenuOpen] = useState(false);
     let navigate = useNavigate()
-    const [isActive, setIsActive] = useState(true)
+    const [isActive, setIsActive] = useState(false)
 
     const toggleMenu = () => {
         setMenuOpen(!open);
@@ -48,6 +54,37 @@ export function Header({ balance, profile, role, username, setMoney, idUser }: H
     if (role === 'user') {
         return (
             <>
+            <button onClick={() => setIsActive(!isActive)} className={isActive ? "mobile-button-hide mobile-button" : "mobile-button"}><LuMenu /></button>
+            <header className={isActive ? "header-responsive" : "header-responsive header-responsive-hide"}>            
+                    <div className='nav-responsive'>
+                    <button onClick={() => setIsActive(!isActive)} className='close-button-responsive'><IoIosClose /></button>
+                    <Link to="/"><img className='spinner spinner-responsive' src="./src/assets/images/min_icon3.png" alt='Imagen Ruleta' /></Link>
+                            <a onClick={() => setModalOpen(true)} className='balance balance-responsive'>
+                                    $ {balance} <img className='balance-img' src="../../src/assets/images/mp-logo-color.png" alt="" />
+                            </a>
+                            <div className='items-responsive'>
+                            <Link to="/leaderboard" className="nav-item-responsive" onClick={() => setIsActive(!isActive)}>
+                                    <div className='icon-text'> 
+                                        <MdLeaderboard /> <p> Leaderboard </p>
+                                    </div> 
+                            
+                            </Link>
+                            <div className='items-responsive items2-responsive'>
+                                <Link to={profile} className='nav-item-responsive' onClick={() => setIsActive(!isActive)}> 
+                                    <div className='icon-text'> 
+                                        <IoPersonSharp /> <p> Profile </p>
+                                    </div> 
+                                </Link>
+                                <button className='nav-item-responsive' onClick={() => { handleLogout(); setIsActive(!isActive)}} > 
+                                    <div className='icon-text'> 
+                                        <FiLogOut /> <p> Log Out </p>
+                                    </div>
+                                </button>
+                            </div>
+                            </div>
+                    </div>
+                </header>
+
                 <div className='nav-item menu-open'>
                     <HeaderMenu open={open} username={username} profile={profile} />
                 </div>
@@ -76,6 +113,42 @@ export function Header({ balance, profile, role, username, setMoney, idUser }: H
     } if (role === 'admin'){
         return (
         <>
+        <button onClick={() => setIsActive(!isActive)} className={isActive ? "mobile-button-hide mobile-button" : "mobile-button"}><LuMenu /></button>
+        <header className={isActive ? "header-responsive" : "header-responsive header-responsive-hide"}>            
+            <div className='nav-responsive'>
+            <button onClick={() => setIsActive(!isActive)} className='close-button-responsive'><IoIosClose /></button>
+            <Link to="/"><img className='spinner spinner-responsive' src="./src/assets/images/min_icon3.png" alt='Imagen Ruleta' /></Link>
+                    <div className='items-responsive'>
+                    <Link to="/leaderboard" className="nav-item-responsive" onClick={() => setIsActive(!isActive)}>
+                            <div className='icon-text'> 
+                                <MdLeaderboard /> <p> Leaderboard </p>
+                            </div> 
+                    
+                    </Link>
+
+                    <Link to="/userlist" className="nav-item-responsive" onClick={() => setIsActive(!isActive)}>
+                            <div className='icon-text'> 
+                            <MdOutlineAdminPanelSettings /> <p> UserList </p>
+                            </div> 
+                    
+                    </Link>
+
+                    <div className='items-responsive items2-responsive'>
+                        <Link to={profile} className='nav-item-responsive' onClick={() => setIsActive(!isActive)}> 
+                            <div className='icon-text'> 
+                                <IoPersonSharp /> <p> Profile </p>
+                            </div> 
+                        </Link>
+                        <button className='nav-item-responsive' onClick={() => {handleLogout(); setIsActive(!isActive)}} > 
+                            <div className='icon-text'> 
+                                <FiLogOut /> <p> Log Out </p>
+                            </div>
+                        </button>
+                    </div>
+                    </div>
+            </div>
+        </header>
+
         <div className='nav-item menu-open'>
             <HeaderMenu open={open} username={username} profile={profile} />
         </div>
@@ -98,36 +171,27 @@ export function Header({ balance, profile, role, username, setMoney, idUser }: H
     else {
         return (
             <>
-            <button onClick={() => setIsActive(!isActive)} className={isActive ? "invisible mobile-button" : "mobile-button"}>soy el boton del header</button>
-            <header className={isActive ? "header-responsive" : "header-responsive invisible"}>            
+            <button onClick={() => setIsActive(!isActive)} className={isActive ? "mobile-button-hide mobile-button" : "mobile-button"}><LuMenu /></button>
+            <header className={isActive ? "header-responsive" : "header-responsive header-responsive-hide"}>            
                     <div className='nav-responsive'>
-                    <button onClick={() => setIsActive(!isActive)}>hola</button>
+                    <button onClick={() => setIsActive(!isActive)} className='close-button-responsive'><IoIosClose /></button>
                     <Link to="/"><img className='spinner spinner-responsive' src="./src/assets/images/min_icon3.png" alt='Imagen Ruleta' /></Link>
-                            <a onClick={() => setModalOpen(true)} className='balance balance-responsive'>
-                                    $ {balance} <img className='balance-img' src="../../src/assets/images/mp-logo-color.png" alt="" />
-                            </a>
+
                             <div className='items-responsive'>
-                            <Link to="/leaderboard" className="nav-item-responsive">
-                                    <div className='icon-text'> 
-                                        <MdLeaderboard /> <p> Leaderboard </p>
-                                    </div> 
-                            
-                            </Link>
+                            <Link to="/about-us" className="nav-item-responsive" onClick={() => setIsActive(!isActive)}>
+                            <div className='icon-text'>
+                            <GrCircleInformation />
+                            <p> About Us </p>
+                                
+                            </div></Link>
+
                             <div className='items-responsive items2-responsive'>
-                                <Link to={profile} className='nav-item-responsive'> 
-                                    <div className='icon-text'> 
-                                        <IoPersonSharp /> <p> Profile </p>
-                                    </div> 
-                                </Link>
-                                <button className='nav-item-responsive' onClick={handleLogout}> 
-                                    <div className='icon-text'> 
-                                        <FiLogOut /> <p> Log Out </p>
-                                    </div>
-                                </button>
+                                 <button className='gold-button gold-button-responsive' onClick={() => { setModalOpen(true); setIsActive(!isActive)}}>LogIn</button>
                             </div>
                             </div>
                     </div>
                 </header>
+
                 <header className='header'>
                 {modalOpen && <LogIn onClose={handleModalClose} />}
                 <nav className='nav'>
