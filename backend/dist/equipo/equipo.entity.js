69e2686c8113d3e1d132f08b4503b041984e9fdf
Juan Pablo Morales <jpgpsy01@gmail.com>
2024-11-23 17:47:52 -0300

    retoques

--- backend/dist/equipo/equipo.entity.js
+++ backend/dist/equipo/equipo.entity.js
@@ -11,7 +11,7 @@ import { Cascade, Collection, Entity, ManyToMany, ManyToOne, PrimaryKey, } from 
 import { Participante } from "../participante/participante.entity.js";
 import { Partido } from "../partido/partido.entity.js";
 import { Torneo } from "../torneo/torneo.entity.js";
-let Equipo = class Equipo {
+export let Equipo = class Equipo {
     constructor() {
         this.participantes = new Collection(this);
         this.partidos = new Collection(this);
@@ -42,5 +42,4 @@ __decorate([
 Equipo = __decorate([
     Entity()
 ], Equipo);
-export { Equipo };
 //# sourceMappingURL=equipo.entity.js.map
