b937c014267571333bfb8a3ab3d93b864aabdffe
Martinalbertengo <tincho9704@gmail.com>
2025-04-16 11:52:37 -0300

    Error fixed

--- api/configs/config.js
+++ api/configs/config.js
@@ -10,9 +10,9 @@ module.exports = {
     signature: process.env.JWT_SIGNATURE
   },
   mysql: {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'sys'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DB
   }
 }
