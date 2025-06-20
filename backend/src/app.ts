dc9647774fe130878776f6b073ef20aeb9e31e77
Agustin Dana <agusidana04@gmail.com>
2025-06-20 16:25:31 -0300

    pusheando

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,8 @@
-<<<<<<< HEAD
 import express, { Request, Response, NextFunction } from 'express';
+import { usuarioRouter } from './usuario/usuario.route.js';
+import { tareaRouter } from './tarea/tarea.route.js';
+import { servicioRouter } from './servicio/servicio.route.js';
+import { turnoRouter } from './turno/turno.route.js';
 import 'reflect-metadata';
 import { orm, syncSchema } from './shared/db/orm.js';
 import { RequestContext } from '@mikro-orm/core';
@@ -14,6 +17,10 @@ app.use((req, res, next: NextFunction) => {
 });
 
 app.use('/api/serviceTypes', serviceTypeRouter);
+app.use('/api/usuario', usuarioRouter);
+app.use('/api/tarea', tareaRouter);
+app.use('/api/servicio', servicioRouter);
+app.use('/api/turno', turnoRouter);
 
 app.use((_, res) => {
   return res.status(404).send({ message: 'Resource not found' });
@@ -21,41 +28,10 @@ app.use((_, res) => {
 
 await syncSchema(); //nunca en producción
 
-app.listen(3006, () => {
+/* app.listen(3006, () => {
   console.log('Server runnning on http://localhost:3006/');
-});
-=======
-import 'reflect-metadata'
-import express from 'express'
-import { usuarioRouter }  from './usuario/usuario.route.js';
-import { tareaRouter } from './tarea/tarea.route.js'
-import { orm, syncSchema } from './shared/db/orm.js'
-import { RequestContext } from '@mikro-orm/core'
-import { servicioRouter } from './servicio/servicio.route.js';
-import { turnoRouter } from './turno/turno.route.js'
-
-const app = express()
-app.use(express.json())
-
-//luego de los middlewares base
-app.use((req, res, next) => {
-  RequestContext.create(orm.em, next)
-})
-//antes de las rutas y middlewares de negocio
-
-app.use('/api/usuario', usuarioRouter)
-app.use('/api/tarea', tareaRouter)
-app.use('/api/servicio', servicioRouter)
-app.use('/api/turno',turnoRouter)
-
-app.use((req, res, next) => {
-  res.status(404).send({ message: 'Resource not found' });
-});
-
-
-await syncSchema() //never in production
+}); */
 
 app.listen(3000, () => {
-  console.log('Server runnning on http://localhost:3000/')
-})
->>>>>>> origin/luis
+  console.log('Server runnning on http://localhost:3000/asdasdasdasdasd');
+});
